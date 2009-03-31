1bdf8d1be075090d3be0e65ffe459e57c9582fce
Dair Grant <dair@refnum.com>
2009-03-31 23:18:05 +0100

    Added JavaScript.

---
+++ Documentation/html/nano.js
@@ -0,0 +1,25 @@
+//============================================================================
+//		Window load
+//----------------------------------------------------------------------------
+window.onload = function()
+{
+
+
+	// Apply the corners
+	settings = {	tl: { radius: 6 },
+					tr: { radius: 6 },
+					bl: { radius: 6 },
+					br: { radius: 6 },
+					antiAlias: true,
+					autoPad:   true };
+
+	theCorners = new curvyCorners(settings, "box");
+	theCorners.applyCornersToAll();
+
+	theCorners = new curvyCorners(settings, "box notice");
+	theCorners.applyCornersToAll();
+		
+	theCorners = new curvyCorners(settings, "box code");
+	theCorners.applyCornersToAll();
+}
+
