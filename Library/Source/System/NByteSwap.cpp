17f902c9a3a5d790156b002d8b2889798aebd92d
Dair Grant <dair@refnum.com>
2009-04-03 19:53:38 +0100

    Added byte swapping methods.

--- Library/Source/System/NByteSwap.cpp
+++ Library/Source/System/NByteSwap.cpp
@@ -19,3 +19,56 @@
 
 
 
+
+//============================================================================
+//		NByteSwap::SwapInt16 : Swap a 16-bit value.
+//----------------------------------------------------------------------------
+void NByteSwap::SwapInt16(UInt16 *theValue)
+{
+
+
+	// Swap the value
+	*theValue =	(((UInt16) ((*theValue) & 0xFF00)) >> 8) |
+				(((UInt16) ((*theValue) & 0x00FF)) >> 0);
+}
+
+
+
+
+
+//============================================================================
+//		NByteSwap::SwapInt32 : Swap a 32-bit value.
+//----------------------------------------------------------------------------
+void NByteSwap::SwapInt32(UInt32 *theValue)
+{
+
+
+	// Swap the value
+	*theValue =	(((UInt32) ((*theValue) & 0xFF000000)) >> 24) |
+				(((UInt32) ((*theValue) & 0x00FF0000)) >> 16) |
+				(((UInt32) ((*theValue) & 0x0000FF00)) >>  8) |
+				(((UInt32) ((*theValue) & 0x000000FF)) >>  0);
+}
+
+
+
+
+
+//============================================================================
+//		NByteSwap::SwapInt64 : Swap a 64-bit value.
+//----------------------------------------------------------------------------
+void NByteSwap::SwapInt64(UInt64 *theValue)
+{
+
+
+	// Swap the value
+	*theValue =	(((UInt64) ((*theValue) & 0xFF00000000000000ULL)) >> 56) |
+				(((UInt64) ((*theValue) & 0x00FF000000000000ULL)) >> 48) |
+				(((UInt64) ((*theValue) & 0x0000FF0000000000ULL)) >> 40) |
+				(((UInt64) ((*theValue) & 0x000000FF00000000ULL)) >> 32) |
+				(((UInt64) ((*theValue) & 0x00000000FF000000ULL)) >> 24) |
+				(((UInt64) ((*theValue) & 0x0000000000FF0000ULL)) >> 16) |
+				(((UInt64) ((*theValue) & 0x000000000000FF00ULL)) >>  8) |
+				(((UInt64) ((*theValue) & 0x00000000000000FFULL)) >>  0);
+}
+
--- Library/Source/System/NByteSwap.h
+++ Library/Source/System/NByteSwap.h
@@ -41,9 +41,69 @@ static const EndianFormat kEndianNative								= (NN_TARGET_ENDIAN_BIG ? kEndian
 //----------------------------------------------------------------------------
 class NByteSwap {
 public:
-
-
+	// Swap primitives
+	static void							SwapInt16(UInt16 *theValue);
+	static void							SwapInt32(UInt32 *theValue);
+	static void							SwapInt64(UInt64 *theValue);
 };
+
+
+
+
+
+//============================================================================
+//		Inline functions
+//----------------------------------------------------------------------------
+#if TARGET_RT_BIG_ENDIAN
+	inline UInt16 SwapUInt16_BtoN(UInt16 theValue)		{ return(theValue); }
+	inline UInt32 SwapUInt32_BtoN(UInt32 theValue)		{ return(theValue); }
+	inline UInt64 SwapUInt64_BtoN(UInt64 theValue)		{ return(theValue); }
+
+	inline SInt16 SwapSInt16_BtoN(SInt16 theValue)		{ return(theValue); }
+	inline SInt32 SwapSInt32_BtoN(SInt32 theValue)		{ return(theValue); }
+	inline SInt64 SwapSInt64_BtoN(SInt64 theValue)		{ return(theValue); }
+
+	inline Float32 SwapFloat32_BtoN(Float32 theValue)	{ return(theValue); }
+	inline Float64 SwapFloat64_BtoN(Float64 theValue)	{ return(theValue); }
+
+
+	inline UInt16 SwapUInt16_LtoN(UInt16 theValue)		{ NByteSwap::SwapInt16(&theValue);				return(theValue); }
+	inline UInt32 SwapUInt32_LtoN(UInt32 theValue)		{ NByteSwap::SwapInt32(&theValue);				return(theValue); }
+	inline UInt64 SwapUInt64_LtoN(UInt64 theValue)		{ NByteSwap::SwapInt64(&theValue);				return(theValue); }
+
+	inline SInt16 SwapSInt16_LtoN(SInt16 theValue)		{ NByteSwap::SwapInt16((UInt16 *) &theValue);	return(theValue); }
+	inline SInt32 SwapSInt32_LtoN(SInt32 theValue)		{ NByteSwap::SwapInt32((UInt32 *) &theValue);	return(theValue); }
+	inline SInt64 SwapSInt64_LtoN(SInt64 theValue)		{ NByteSwap::SwapInt64((UInt64 *) &theValue);	return(theValue); }
+
+	inline Float32 SwapFloat32_LtoN(Float32 theValue)	{ NByteSwap::SwapInt32((UInt32 *) &theValue);	return(theValue); }
+	inline Float64 SwapFloat64_LtoN(Float64 theValue)	{ NByteSwap::SwapInt64((UInt64 *) &theValue);	return(theValue); }
+#endif
+
+
+#if TARGET_RT_LITTLE_ENDIAN
+	inline UInt16 SwapUInt16_BtoN(UInt16 theValue)		{ NByteSwap::SwapInt16(&theValue);				return(theValue); }
+	inline UInt32 SwapUInt32_BtoN(UInt32 theValue)		{ NByteSwap::SwapInt32(&theValue);				return(theValue); }
+	inline UInt64 SwapUInt64_BtoN(UInt64 theValue)		{ NByteSwap::SwapInt64(&theValue);				return(theValue); }
+
+	inline SInt16 SwapSInt16_BtoN(SInt16 theValue)		{ NByteSwap::SwapInt16((UInt16 *) &theValue);	return(theValue); }
+	inline SInt32 SwapSInt32_BtoN(SInt32 theValue)		{ NByteSwap::SwapInt32((UInt32 *) &theValue);	return(theValue); }
+	inline SInt64 SwapSInt64_BtoN(SInt64 theValue)		{ NByteSwap::SwapInt64((UInt64 *) &theValue);	return(theValue); }
+
+	inline Float32 SwapFloat32_BtoN(Float32 theValue)	{ NByteSwap::SwapInt32((UInt32 *) &theValue);	return(theValue); }
+	inline Float64 SwapFloat64_BtoN(Float64 theValue)	{ NByteSwap::SwapInt64((UInt64 *) &theValue);	return(theValue); }
+
+
+	inline UInt16 SwapUInt16_LtoN(UInt16 theValue)		{ return(theValue); }
+	inline UInt32 SwapUInt32_LtoN(UInt32 theValue)		{ return(theValue); }
+	inline UInt64 SwapUInt64_LtoN(UInt64 theValue)		{ return(theValue); }
+
+	inline SInt16 SwapSInt16_LtoN(SInt16 theValue)		{ return(theValue); }
+	inline SInt32 SwapSInt32_LtoN(SInt32 theValue)		{ return(theValue); }
+	inline SInt64 SwapSInt64_LtoN(SInt64 theValue)		{ return(theValue); }
+
+	inline Float32 SwapFloat32_LtoN(Float32 theValue)	{ return(theValue); }
+	inline Float64 SwapFloat64_LtoN(Float64 theValue)	{ return(theValue); }
+#endif
 
 
 
