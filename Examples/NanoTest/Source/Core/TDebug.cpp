fee33a9f6a5d8484513b9b4ae7a4534b98a3861f
Dair Grant <dair@refnum.com>
2009-04-06 18:00:16 +0100

    Added debug tests.

---
+++ Examples/NanoTest/Source/Core/TDebug.cpp
@@ -0,0 +1,34 @@
+/*	NAME:
+		TDebug.cpp
+
+	DESCRIPTION:
+		NDebug tests.
+
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "NDebug.h"
+
+#include "TDebug.h"
+
+
+
+
+
+//============================================================================
+//		TDebug::Execute : Execute the tests.
+//----------------------------------------------------------------------------
+void TDebug::Execute(void)
+{
+
+
+	// Execute the tests
+}
+
