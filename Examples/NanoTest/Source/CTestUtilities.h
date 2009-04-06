165047251fe78db33ae6fedd99fdf8e104b848b3
Dair Grant <dair@refnum.com>
2009-04-06 18:01:05 +0100

    Missed from previous checkin.

---
+++ Examples/NanoTest/Source/CTestUtilities.h
@@ -0,0 +1,46 @@
+/*	NAME:
+		CTestUtilities.h
+
+	DESCRIPTION:
+		NDebug tests.
+
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+#ifndef CTESTUTILITIES_HDR
+#define CTESTUTILITIES_HDR
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "NString.h"
+
+
+
+
+
+//============================================================================
+//		Class declaration
+//----------------------------------------------------------------------------
+class CTestUtilities {
+public:
+	// Enable/disable debug capture
+	static NString						SetDebugCapture(bool captureDebug);
+
+
+private:
+	static void							DebugOutputHook(const char *theMsg);
+
+
+private:
+	static NString						mDebugOutput;
+};
+
+
+
+
+
+#endif // CTESTUTILITIES_HDR
