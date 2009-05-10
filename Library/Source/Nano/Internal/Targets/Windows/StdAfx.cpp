1c7c5e5dfe5471e1bdca71e092e2126a1b125c92
Dair Grant <dair@refnum.com>
2009-05-10 20:41:53 +0100

    Enabled VC++ precompiled headers.

---
+++ Library/Source/Nano/Internal/Targets/Windows/StdAfx.cpp
@@ -0,0 +1,17 @@
+/*	NAME:
+		StdAfx.cpp
+
+	DESCRIPTION:
+		Windows precompiled header.
+	
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "StdAfx.h"
