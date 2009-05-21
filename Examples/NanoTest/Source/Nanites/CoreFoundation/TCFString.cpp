28dd82bb55889bad58efea457743bb804c67dab7
Dair Grant <dair@refnum.com>
2009-05-21 17:12:58 +0100

    Added NCFString tests.

--- Examples/NanoTest/Source/Nanites/CoreFoundation/TCFString.cpp
+++ Examples/NanoTest/Source/Nanites/CoreFoundation/TCFString.cpp
@@ -23,10 +23,36 @@
 
 
 //============================================================================
+//		Internal constants
+//----------------------------------------------------------------------------
+static const NString     kNStringValue						= "Hello World";
+static const CFStringRef kCFStringValue						= CFSTR("Hello World");
+
+
+
+
+
+//============================================================================
 //		TCFString::Execute : Execute the tests.
 //----------------------------------------------------------------------------
 void TCFString::Execute(void)
-{
+{	NCFString		theString;
+	CFStringRef		cfString;
+
+
+
 	// Execute the tests
+	theString = kNStringValue;
+	cfString  = theString;
+	NN_ASSERT(CFStringCompare(cfString, kCFStringValue, 0) == kCFCompareEqualTo);
+
+	theString = NCFString(kCFStringValue, false);
+	NN_ASSERT(theString == kNStringValue);
+
+	theString.Set(kCFStringValue, false);
+	NN_ASSERT(theString == kNStringValue);
+
+	theString.Set(CFStringCreateCopy(kCFAllocatorDefault, kCFStringValue), true);
+	NN_ASSERT(theString == kNStringValue);
 }
 
