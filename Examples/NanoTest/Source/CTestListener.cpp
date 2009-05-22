59ea7a66448f5463866f1842d87b2e97db70538b
Dair Grant <dair@refnum.com>
2009-05-22 21:21:00 +0100

    Added broadcaster/listener tests.

---
+++ Examples/NanoTest/Source/CTestListener.cpp
@@ -0,0 +1,74 @@
+/*	NAME:
+		CTestListener.cpp
+
+	DESCRIPTION:
+		Test listener.
+
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "CTestListener.h"
+
+
+
+
+
+//============================================================================
+//		CTestListener::CTestListener : Constructor.
+//----------------------------------------------------------------------------
+CTestListener::CTestListener(void)
+{
+
+
+	// Initialise ourselves
+	mTarget = NULL;
+}
+
+
+
+
+
+//============================================================================
+//		CTestListener::~CTestListener : Destructor.
+//----------------------------------------------------------------------------
+CTestListener::~CTestListener(void)
+{
+}
+
+
+
+
+
+//============================================================================
+//		CTestListener::SetTarget : Set the target.
+//----------------------------------------------------------------------------
+void CTestListener::SetTarget(BroadcastMsg *theTarget)
+{
+
+
+	// Set the target
+	mTarget = theTarget;
+}
+
+
+
+
+
+//============================================================================
+//		CTestListener::DoMessage : Handle messages.
+//----------------------------------------------------------------------------
+void CTestListener::DoMessage(BroadcastMsg theMsg, void */*msgData*/)
+{
+
+
+	// Update the target
+	if (mTarget != NULL)
+		*mTarget = theMsg;
+}
--- Examples/NanoTest/Source/Nano/Events/TBroadcaster.cpp
+++ Examples/NanoTest/Source/Nano/Events/TBroadcaster.cpp
@@ -14,8 +14,9 @@
 //============================================================================
 //		Include files
 //----------------------------------------------------------------------------
-#include "NBroadcaster.h"
+#include "NListener.h"
 
+#include "CTestListener.h"
 #include "TBroadcaster.h"
 
 
@@ -26,10 +27,82 @@
 //		TBroadcaster::Execute : Execute the tests.
 //----------------------------------------------------------------------------
 void TBroadcaster::Execute(void)
-{
+{	NBroadcaster		theBroadcaster, theBroadcaster2;
+	CTestListener		theListener, theListener2;
+	BroadcastMsg		theMsg;
 
 
-	// Execute the tests
+
+	// Initialise ourselves
+	theMsg = kTestMsgNone;
+	theListener.SetTarget(&theMsg);
+
+
+
+	// State
+	NN_ASSERT( theBroadcaster.IsBroadcasting());
+	NN_ASSERT(!theBroadcaster.HasListeners());
+
+
+
+	// Add/remove listener
+	theBroadcaster.AddListener(&theListener);
+	NN_ASSERT(theBroadcaster.HasListeners());
+
+	theBroadcaster.RemoveListener(&theListener);
+	NN_ASSERT(!theBroadcaster.HasListeners());
+
+
+
+	// Remove all listeners
+	theBroadcaster.AddListener(&theListener);
+	theBroadcaster.AddListener(&theListener2);
+	NN_ASSERT(theBroadcaster.HasListeners());
+
+	theBroadcaster.RemoveListeners();
+	NN_ASSERT(!theBroadcaster.HasListeners());
+
+
+
+	// Broadcast
+	theBroadcaster.AddListener(&theListener);
+	theBroadcaster.BroadcastMessage(kTestMsgValue);
+
+	NN_ASSERT(theMsg == kTestMsgValue);
+
+
+
+	// No broadcast
+	theMsg = kTestMsgNone;
+
+	theBroadcaster.SetBroadcasting(false);
+	theBroadcaster.BroadcastMessage(kTestMsgValue);
+	theBroadcaster.SetBroadcasting(true);
+
+	NN_ASSERT(theMsg == kTestMsgNone);
+
+
+
+	// No listeners
+	theMsg = kTestMsgNone;
+
+	theBroadcaster.RemoveListeners();
+	theBroadcaster.BroadcastMessage(kTestMsgValue);
+
+	NN_ASSERT(theMsg == kTestMsgNone);
+
+
+
+	// Copy listeners
+	theMsg = kTestMsgNone;
+	theBroadcaster.AddListener(&theListener);
+	
+	theBroadcaster2 = theBroadcaster;
+	theBroadcaster.RemoveListeners();
+	
+	theBroadcaster2.BroadcastMessage(kTestMsgValue);
+
+	NN_ASSERT(theMsg == kTestMsgValue);
 }
 
 
