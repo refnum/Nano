f95900545f058c10448a420674aad6cf3df7db3f
Dair Grant <dair@refnum.com>
2009-05-28 21:47:36 +0100

    Added NByteSwap tests.

--- Examples/NanoTest/Source/Nano/System/TByteSwap.cpp
+++ Examples/NanoTest/Source/Nano/System/TByteSwap.cpp
@@ -14,6 +14,7 @@
 //============================================================================
 //		Include files
 //----------------------------------------------------------------------------
+#include "NMathUtilities.h"
 #include "NByteSwap.h"
 
 #include "TByteSwap.h"
@@ -23,10 +24,126 @@
 
 
 //============================================================================
+//		Constants
+//----------------------------------------------------------------------------
+static const UInt16 kValue16									= 0x1234;
+static const UInt32 kValue32									= 0x12345678;
+static const UInt64 kValue64									= 0x123456789ABCDEF0ULL;
+
+static const UInt16 kSwapped16									= 0x3412;
+static const UInt32 kSwapped32									= 0x78563412;
+static const UInt64 kSwapped64									= 0xF0DEBC9A78563412ULL;
+
+static const UInt8 kFloat32L[4]									= { 0xDB, 0x0F, 0x49, 0x40 };
+static const UInt8 kFloat64L[8]									= { 0x18, 0x2D, 0x44, 0x54, 0xFB, 0x21, 0x09, 0x40 };
+
+static const UInt8 kFloat32B[4]									= { 0x40, 0x49, 0x0F, 0xDB };
+static const UInt8 kFloat64B[8]									= { 0x40, 0x09, 0x21, 0xFB,	0x54, 0x44,	0x2D, 0x18 };
+
+
+
+
+
+//============================================================================
 //		TByteSwap::Execute : Execute the tests.
 //----------------------------------------------------------------------------
 void TByteSwap::Execute(void)
-{
+{	Float32		float32N, float32B, float32L;
+	Float64		float64N, float64B, float64L;
+	UInt16		value16;
+	UInt32		value32;
+	UInt64		value64;
+
+
+
+	// Get the state we need
+	memcpy(&float32N, NN_TARGET_ENDIAN_BIG ? &kFloat32B : &kFloat32L, sizeof(Float32));
+	memcpy(&float64N, NN_TARGET_ENDIAN_BIG ? &kFloat64B : &kFloat64L, sizeof(Float64));
+
+	memcpy(&float32B, &kFloat32B, sizeof(Float32));
+	memcpy(&float64B, &kFloat64B, sizeof(Float64));
+
+	memcpy(&float32L, &kFloat32L, sizeof(Float32));
+	memcpy(&float64L, &kFloat64L, sizeof(Float64));
+
+
+
+	// Constants
+	if (NN_TARGET_ENDIAN_BIG)
+		NN_ASSERT(kEndianNative == kEndianBig);
+	else
+		NN_ASSERT(kEndianNative == kEndianLittle);
+
+
+
+	// Primitives
+	value16 = kValue16;
+	value32 = kValue32;
+	value64 = kValue64;
+	
+	NByteSwap::SwapInt16(&value16);
+	NByteSwap::SwapInt32(&value32);
+	NByteSwap::SwapInt64(&value64);
+
+	NN_ASSERT(value16 == kSwapped16);
+	NN_ASSERT(value32 == kSwapped32);
+	NN_ASSERT(value64 == kSwapped64);
+
+
+
+	// Native to big
+	NN_ASSERT(SwapUInt16_NtoB(kValue16) == (UInt16) (NN_TARGET_ENDIAN_BIG ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapUInt32_NtoB(kValue32) == (UInt32) (NN_TARGET_ENDIAN_BIG ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapUInt64_NtoB(kValue64) == (UInt64) (NN_TARGET_ENDIAN_BIG ? kValue64 : kSwapped64));
+
+	NN_ASSERT(SwapSInt16_NtoB(kValue16) == (SInt16) (NN_TARGET_ENDIAN_BIG ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapSInt32_NtoB(kValue32) == (SInt32) (NN_TARGET_ENDIAN_BIG ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapSInt64_NtoB(kValue64) == (SInt64) (NN_TARGET_ENDIAN_BIG ? kValue64 : kSwapped64));
+
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat32_NtoB(float32N), float32B));
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat64_NtoB(float64N), float64B));
+
+
+
+	// Native to little
+	NN_ASSERT(SwapUInt16_NtoL(kValue16) == (UInt16) (NN_TARGET_ENDIAN_LITTLE ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapUInt32_NtoL(kValue32) == (UInt32) (NN_TARGET_ENDIAN_LITTLE ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapUInt64_NtoL(kValue64) == (UInt64) (NN_TARGET_ENDIAN_LITTLE ? kValue64 : kSwapped64));
+
+	NN_ASSERT(SwapSInt16_NtoL(kValue16) == (SInt16) (NN_TARGET_ENDIAN_LITTLE ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapSInt32_NtoL(kValue32) == (SInt32) (NN_TARGET_ENDIAN_LITTLE ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapSInt64_NtoL(kValue64) == (SInt64) (NN_TARGET_ENDIAN_LITTLE ? kValue64 : kSwapped64));
+
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat32_NtoL(float32N), float32L));
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat64_NtoL(float64N), float64L));
+
+
+
+	// Big to native
+	NN_ASSERT(SwapUInt16_BtoN(kValue16) == (UInt16) (NN_TARGET_ENDIAN_BIG ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapUInt32_BtoN(kValue32) == (UInt32) (NN_TARGET_ENDIAN_BIG ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapUInt64_BtoN(kValue64) == (UInt64) (NN_TARGET_ENDIAN_BIG ? kValue64 : kSwapped64));
+
+	NN_ASSERT(SwapSInt16_BtoN(kValue16) == (SInt16) (NN_TARGET_ENDIAN_BIG ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapSInt32_BtoN(kValue32) == (SInt32) (NN_TARGET_ENDIAN_BIG ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapSInt64_BtoN(kValue64) == (SInt64) (NN_TARGET_ENDIAN_BIG ? kValue64 : kSwapped64));
+
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat32_BtoN(float32B), float32N));
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat64_BtoN(float64B), float64N));
+
+
+
+	// Little to native
+	NN_ASSERT(SwapUInt16_LtoN(kValue16) == (UInt16) (NN_TARGET_ENDIAN_LITTLE ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapUInt32_LtoN(kValue32) == (UInt32) (NN_TARGET_ENDIAN_LITTLE ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapUInt64_LtoN(kValue64) == (UInt64) (NN_TARGET_ENDIAN_LITTLE ? kValue64 : kSwapped64));
+
+	NN_ASSERT(SwapSInt16_LtoN(kValue16) == (SInt16) (NN_TARGET_ENDIAN_LITTLE ? kValue16 : kSwapped16));
+	NN_ASSERT(SwapSInt32_LtoN(kValue32) == (SInt32) (NN_TARGET_ENDIAN_LITTLE ? kValue32 : kSwapped32));
+	NN_ASSERT(SwapSInt64_LtoN(kValue64) == (SInt64) (NN_TARGET_ENDIAN_LITTLE ? kValue64 : kSwapped64));
+
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat32_LtoN(float32L), float32N));
+	NN_ASSERT(NMathUtilities::AreEqual(SwapFloat64_LtoN(float64L), float64N));
 }
 
 
