e586e7cee85fe94d3882cbc0c6fea6f025bdccb7
Dair Grant <dair@refnum.com>
2009-06-14 10:43:02 +0100

    Added Qt Nanite.

---
+++ Library/Source/Nanites/Qt/NQObject.cpp
@@ -0,0 +1,31 @@
+/*	NAME:
+		NQObject.cpp
+
+	DESCRIPTION:
+		QObject support.
+	
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "NQObject.h"
+
+
+
+
+
+//============================================================================
+//		Linker
+//----------------------------------------------------------------------------
+void NQObject_SuppressNoCodeLinkerWarning(void);
+void NQObject_SuppressNoCodeLinkerWarning(void)
+{
+}
+
+
---
+++ Library/Source/Nanites/Qt/NQObject.h
@@ -0,0 +1,34 @@
+/*	NAME:
+		NQObject.h
+
+	DESCRIPTION:
+		QObject support.
+	
+	COPYRIGHT:
+		Copyright (c) 2006-2009, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+#ifndef NQOBJECT_HDR
+#define NQOBJECT_HDR
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+
+
+
+
+
+//============================================================================
+//		Macros
+//----------------------------------------------------------------------------
+// Connect a signal
+#define ConnectSelf(_src, _signal, _slot)				connect(_src, SIGNAL(_signal), this, SLOT(_slot))
+
+
+
+#endif // NQOBJECT_HDR
+
+
