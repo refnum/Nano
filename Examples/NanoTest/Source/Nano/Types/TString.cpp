90b1011f99592257d2ecb9cbecbf71fdc5534e06
Dair Grant <dair@refnum.com>
2009-07-03 20:44:30 +0100

    Test whitespace trimming.

--- Examples/NanoTest/Source/Nano/Types/TString.cpp
+++ Examples/NanoTest/Source/Nano/Types/TString.cpp
@@ -272,6 +272,19 @@ void TString::Execute(void)
 
 
 	// Trim
+	testString1 = "\n \tTrim";
+	testString1.TrimLeft();
+	NN_ASSERT(testString1 == "Trim");
+
+	testString1 = "Trim\n \t";
+	testString1.TrimRight();
+	NN_ASSERT(testString1 == "Trim");
+
+	testString1 = "\n \tTrim\t \n";
+	testString1.Trim();
+	NN_ASSERT(testString1 == "Trim");
+
+
 	testString1 = "TrimLeft";
 	testString1.TrimLeft(4);
 	NN_ASSERT(testString1 == "Left");
