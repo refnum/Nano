08b35e124e8a1b44e9a05f660edaf10801a49477
Dair Grant <dair@refnum.com>
2010-01-21 14:58:38 +0000

    Added Qt/Nano conversion functions

--- Library/Source/Nanites/Qt/NQt.h
+++ Library/Source/Nanites/Qt/NQt.h
@@ -16,6 +16,10 @@
 //============================================================================
 //		Include files
 //----------------------------------------------------------------------------
+#include "NRectangle.h"
+#include "NPoint.h"
+#include "NSize.h"
+#include "NString.h"
 
 
 
@@ -24,7 +28,64 @@
 //============================================================================
 //		Inline functions
 //----------------------------------------------------------------------------
+// Nano to Qt
+inline QSizeF ToNS(const NSize &theSize)
+{
+	return(QSizeF(theSize.width, theSize.height));
+}
 
+inline QPointF ToNS(const NPoint &thePoint)
+{
+	return(QPointF(thePoint.x, thePoint.y));
+}
+
+inline QRectF ToNS(const NRectangle &theRect)
+{
+	return(QRectF(theRect.origin.x, theRect.origin.y, theRect.size.width, theRect.size.height));
+}
+
+inline QString ToQt(const NString &theString)
+{
+	return(QString::fromUtf8(theString.GetUTF8()));
+}
+
+
+
+// Qt to Nano
+inline NPoint ToNN(const QPoint &thePoint)
+{
+	return(NPoint(thePoint.x(), thePoint.y()));
+}
+
+inline NSize ToNN(const QSize &theSize)
+{
+	return(NSize(theSize.width(), theSize.height()));
+}
+
+inline NRectangle ToNN(const QRect &theRect)
+{
+	return(NRectangle(theRect.x(), theRect.y(), theRect.width(), theRect.height()));
+}
+
+inline NPoint ToNN(const QPointF &thePoint)
+{
+	return(NPoint(thePoint.x(), thePoint.y()));
+}
+
+inline NSize ToNN(const QSizeF &theSize)
+{
+	return(NSize(theSize.width(), theSize.height()));
+}
+
+inline NRectangle ToNN(const QRectF &theRect)
+{
+	return(NRectangle(theRect.x(), theRect.y(), theRect.width(), theRect.height()));
+}
+
+inline NString ToNN(const QString &theString)
+{
+	return(NString(theString.toUtf8().data()));
+}
 
 
 
