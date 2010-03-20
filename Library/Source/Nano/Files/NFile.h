388165835f2ac58ad76f4fbc400a892883f07b0d
Dair Grant <dair@refnum.com>
2010-03-20 12:45:59 +0000

    Added NN_DIR definition for NN_TARGET_IPHONE

--- Library/Source/Nano/Files/NFile.h
+++ Library/Source/Nano/Files/NFile.h
@@ -72,7 +72,7 @@ static const NFileRef kNFileRefNone									= 0;
 
 
 // Directory separator
-#if NN_TARGET_MAC
+#if NN_TARGET_MAC || NN_TARGET_IPHONE
 	#define NN_DIR													"/"
 #elif NN_TARGET_WINDOWS
 	#define NN_DIR													"\\"
