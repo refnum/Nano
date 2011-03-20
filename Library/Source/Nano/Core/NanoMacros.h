485620fdd05322d9bc67dfd14b4356bff45e8b4d
Dair Grant <dair@refnum.com>
2011-03-20 11:26:19 +0000

    Added NN_TEST_BIT

--- Library/Source/Nano/Core/NanoMacros.h
+++ Library/Source/Nano/Core/NanoMacros.h
@@ -44,6 +44,10 @@
 #define NN_ALIGNED_TO(_value, _size)								((((UIntPtr) _value) % _size) == 0)
 
 
+// Is a bit set?
+#define NN_TEST_BIT(_value, _mask)									(((_value) & (_mask)) == (_mask))
+
+
 // Declare a binary constant
 //
 //		 B8(01010101)									= 85
