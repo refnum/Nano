9a47a90491d4e4ca8bcb76ccd2e14ff96a08935a
Dair Grant <dair@refnum.com>
2013-05-15 13:12:34 -0700

    Linux implementation

--- Library/Source/Nano/Internal/Targets/Linux/NLinuxSystem.cpp
+++ Library/Source/Nano/Internal/Targets/Linux/NLinuxSystem.cpp
@@ -14,8 +14,13 @@
 //============================================================================
 //		Include files
 //----------------------------------------------------------------------------
+#include <sys/select.h>
+#include <sys/fcntl.h>
 #include <sys/utsname.h>
+#include <sys/wait.h>
+#include <errno.h>
 
+#include "NFileUtilities.h"
 #include "NTargetSystem.h"
 
 
@@ -23,8 +28,169 @@
 
 
 //============================================================================
+//		Internal constants
+//----------------------------------------------------------------------------
+// Tasks
+static const NIndex kPipeRead											= 0;
+static const NIndex kPipeWrite											= 1;
+
+// Misc
+static const size_t kBufferSize											= 2 * kNKilobyte;
+
+
+
+
+
+//============================================================================
+//		Internal functions
+//----------------------------------------------------------------------------
+//      ExecuteTask : Execute a task.
+//----------------------------------------------------------------------------
+static void ExecuteTask(const NString &theCmd, const NString &cmdName, const NStringList &theArgs)
+{	std::vector<const char *>		argList;
+	NStringListConstIterator		theIter;
+	int								sysErr;
+
+
+
+	// Build the argument list
+	argList.push_back(cmdName.GetUTF8());
+	
+	for (theIter = theArgs.begin(); theIter != theArgs.end(); theIter++)
+		argList.push_back(theIter->GetUTF8());
+	
+	argList.push_back(NULL);
+
+
+
+	// Execute the task
+	sysErr = execv(theCmd.GetUTF8(), const_cast<char* const*>(&argList[0]));
+	NN_ASSERT(sysErr == 0);
+
+	_exit(EXIT_FAILURE);
+}
+
+
+
+
+
+//============================================================================
+//		ClosePipe : Close a pipe.
+//----------------------------------------------------------------------------
+static void ClosePipe(NTaskPipeRef &thePipe)
+{
+
+
+	// Close the pipe
+	if (thePipe != kNTaskPipeRefNone)
+		{
+		close((int) thePipe);
+		thePipe = kNTaskPipeRefNone;
+		}
+}
+
+
+
+
+
+//============================================================================
+//		ReadPipe : Read from a pipe.
+//----------------------------------------------------------------------------
+static NString ReadPipe(NTaskPipeRef &thePipe, bool lastRead)
+{	char		theBuffer[kBufferSize];
+	NString		theString;
+	NIndex		numRead;
+
+
+
+	// Check the pipe
+	if (thePipe == kNTaskPipeRefNone)
+		return(theString);
+
+
+
+	// Read from the pipe
+	do
+		{
+		numRead = (NIndex) read((int) thePipe, theBuffer, kBufferSize);
+		if (numRead >= 1)
+			theString += NString(theBuffer, numRead);
+		}
+	while (numRead >= 1);
+
+
+
+	// Clean up
+	//
+	// If this was the last read from the pipe, close it when done.
+	if (lastRead && numRead == 0)
+		ClosePipe(thePipe);
+
+	return(theString);
+}
+
+
+
+
+
+//============================================================================
+//		WritePipe : Write to a pipe.
+//----------------------------------------------------------------------------
+static void WritePipe(NTaskPipeRef thePipe, const NString &theText)
+{	NData	theData;
+
+
+
+	// Check the pipe
+	if (thePipe == kNTaskPipeRefNone)
+		return;
+	
+	
+	
+	// Write to the pipe
+	theData = theText.GetData();
+	write((int) thePipe, theData.GetData(), theData.GetSize());
+}
+
+
+
+
+
+//============================================================================
+//      GetProcInfoFile : Get the contents of a /proc/xxxxinfo file.
+//----------------------------------------------------------------------------
+static NString GetProcInfoFile(const NString &thePath)
+{	char		theBuffer[kBufferSize];
+	FILE		*theFile;
+	size_t		numRead;
+	NString		theText;
+
+
+
+	// Read the file
+	theFile = fopen(thePath.GetUTF8(), "r");
+	if (theFile != NULL)
+		{
+		numRead = fread(theBuffer, 1, sizeof(theBuffer), theFile);
+		if (numRead > 0 && numRead < sizeof(theBuffer))
+			theText = NString(theBuffer, numRead);
+
+		fclose(theFile);
+		}
+	
+	return(theText);
+}
+
+
+
+
+
+//============================================================================
+//      Public functions
+//----------------------------------------------------------------------------
 //      NTargetSystem::DebugLog : Emit a debug message.
 //----------------------------------------------------------------------------
+#pragma mark -
 void NTargetSystem::DebugLog(const char *theMsg)
 {
 
@@ -165,12 +331,25 @@ NString NTargetSystem::GetOSName(void)
 //		NTargetSystem::GetSystemCPU : Get the clock speed.
 //----------------------------------------------------------------------------
 UInt64 NTargetSystem::GetSystemCPU(void)
-{
+{	NRangeList	theRanges;
+	UInt64		theResult;
+	NString		theInfo;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::GetSystemCPU not implemented!");
-	return(0);
+
+	// Get the state we need
+	theInfo   = GetProcInfoFile("/proc/cpuinfo");
+	theResult = 0;
+
+
+
+	// Get the clock speed
+	theRanges = theInfo.FindAll("cpu MHz\\s*:\\s*(\\d+)", kNStringPattern);
+	if (theRanges.size() >= 2)
+		theResult = NNumber(theInfo.GetString(theRanges[1])).GetUInt64();
+
+	NN_ASSERT(theResult != 0);
+	return(theResult);
 }
 
 
@@ -181,12 +360,25 @@ UInt64 NTargetSystem::GetSystemCPU(void)
 //		NTargetSystem::GetSystemRAM : Get the physical memory.
 //----------------------------------------------------------------------------
 UInt64 NTargetSystem::GetSystemRAM(void)
-{
+{	NRangeList	theRanges;
+	UInt64		theResult;
+	NString		theInfo;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::GetSystemRAM not implemented!");
-	return(0);
+
+	// Get the state we need
+	theInfo   = GetProcInfoFile("/proc/meminfo");
+	theResult = 0;
+
+
+
+	// Get the physical memory
+	theRanges = theInfo.FindAll("MemTotal:\\s*(\\d+)", kNStringPattern);
+	if (theRanges.size() >= 2)
+		theResult = NNumber(theInfo.GetString(theRanges[1])).GetUInt64() * kNKilobyte;
+
+	NN_ASSERT(theResult != 0);
+	return(theResult);
 }
 
 
@@ -197,12 +389,18 @@ UInt64 NTargetSystem::GetSystemRAM(void)
 //		NTargetSystem::GetSystemArch : Get the system architecture.
 //----------------------------------------------------------------------------
 NString NTargetSystem::GetSystemArch(void)
-{
+{	NString		theArch;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::GetSystemArch not implemented!");
-	return("");
+
+	// Get the architecture
+#if defined(__i386__) || defined(__x86_64__)
+	theArch = "x86";
+#else
+	UNKNOWN ARCH
+#endif
+
+	return(theArch);
 }
 
 
@@ -213,12 +411,81 @@ NString NTargetSystem::GetSystemArch(void)
 //      NTargetSystem::TaskCreate : Create a task.
 //----------------------------------------------------------------------------
 TaskInfo NTargetSystem::TaskCreate(const NString &theCmd, const NStringList &theArgs)
-{
+{	int				pipeStdIn[2], pipeStdOut[2], pipeStdErr[2];
+	TaskInfo		theTask;
+	NString			cmdName;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskCreate not implemented!");
-	return(TaskInfo());
+
+	// Validate our state
+	NN_ASSERT(sizeof(NTaskID)      >= sizeof(pid_t));
+	NN_ASSERT(sizeof(NTaskPipeRef) >= sizeof(int));
+
+
+
+	// Get the state we need
+	theTask.taskID     = kNTaskIDNone;
+	theTask.taskResult = 0;
+	
+	theTask.stdIn  = kNTaskPipeRefNone;
+	theTask.stdOut = kNTaskPipeRefNone;
+	theTask.stdErr = kNTaskPipeRefNone;
+
+	cmdName = NFile(theCmd).GetName();
+
+
+
+	// Create the pipes
+	if (pipe(pipeStdIn)  == -1 ||
+		pipe(pipeStdOut) == -1 ||
+		pipe(pipeStdErr) == -1)
+		return(theTask);
+
+
+
+	// Fork the process
+	theTask.taskID = fork();
+	
+	switch ((SInt32) theTask.taskID) {
+		case -1:
+			// Handle failure
+			return(theTask);
+			break;
+
+		case 0:
+			// We're the child
+			//
+			// We read from the stdin pipe and write to the stdout/stderr pipes.
+			close(pipeStdIn [kPipeWrite]);
+			close(pipeStdOut[kPipeRead]);
+			close(pipeStdErr[kPipeRead]);
+
+			dup2(pipeStdIn [kPipeRead],  STDIN_FILENO);
+			dup2(pipeStdOut[kPipeWrite], STDOUT_FILENO);
+			dup2(pipeStdErr[kPipeWrite], STDERR_FILENO);
+
+			ExecuteTask(theCmd, cmdName, theArgs);
+			_exit(1);
+			break;
+
+		default:
+			// We're the parent
+			//
+			// We write to the stdin pipe and read from the stdout/stderr pipes.
+			close(pipeStdIn [kPipeRead]);
+			close(pipeStdOut[kPipeWrite]);
+			close(pipeStdErr[kPipeWrite]);
+			
+			theTask.stdIn  = (NTaskPipeRef) pipeStdIn [kPipeWrite];
+			theTask.stdOut = (NTaskPipeRef) pipeStdOut[kPipeRead];
+			theTask.stdErr = (NTaskPipeRef) pipeStdErr[kPipeRead];
+			
+			fcntl((int) theTask.stdOut, F_SETFL, O_NONBLOCK);
+			fcntl((int) theTask.stdErr, F_SETFL, O_NONBLOCK);
+			break;
+		}
+	
+	return(theTask);
 }
 
 
@@ -232,8 +499,12 @@ void NTargetSystem::TaskDestroy(TaskInfo &theTask)
 {
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskDestroy not implemented!");
+	// Clean up
+	theTask.taskID = kNTaskIDNone;
+	
+	ClosePipe(theTask.stdIn);
+	ClosePipe(theTask.stdOut);
+	ClosePipe(theTask.stdErr);
 }
 
 
@@ -244,11 +515,35 @@ void NTargetSystem::TaskDestroy(TaskInfo &theTask)
 //      NTargetSystem::TaskUpdate : Update a task.
 //----------------------------------------------------------------------------
 void NTargetSystem::TaskUpdate(TaskInfo &theTask)
-{
+{	int		sysErr, theStatus;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskUpdate not implemented!");
+
+	// Get the child status
+	sysErr = waitpid((pid_t) theTask.taskID, &theStatus, WNOHANG);
+	if (sysErr == 0)
+		return;
+
+	else if (sysErr == (int) theTask.taskID && WIFEXITED(theStatus))
+		theStatus = WEXITSTATUS(theStatus);
+
+	else
+		theStatus = ECHILD;
+
+
+
+	// Update the task
+	theTask.taskID     = kNTaskIDNone;
+	theTask.taskResult = theStatus;
+
+
+
+	// Free our resources
+	//
+	// The stdout/err pipes are left open since they may contain buffered data
+	// that can still be read. However since the child has exited, no more data
+	// can be written to it and so the stdin pipe can be closed.
+	ClosePipe(theTask.stdIn);
 }
 
 
@@ -259,12 +554,20 @@ void NTargetSystem::TaskUpdate(TaskInfo &theTask)
 //      NTargetSystem::TaskRead : Read from a task.
 //----------------------------------------------------------------------------
 NString NTargetSystem::TaskRead(TaskInfo &theTask, bool stdErr)
-{
+{	NString		theResult;
+	bool		lastRead;
+	
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskRead not implemented!");
-	return("");
+	// Read from the task
+	lastRead = (theTask.taskID == kNTaskIDNone);
+	
+	if (stdErr)
+		theResult = ReadPipe(theTask.stdErr, lastRead);
+	else
+		theResult = ReadPipe(theTask.stdOut, lastRead);
+	
+	return(theResult);
 }
 
 
@@ -278,8 +581,8 @@ void NTargetSystem::TaskWrite(const TaskInfo &theTask, const NString &theText)
 {
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskWrite not implemented!");
+	// Write to the task
+	WritePipe(theTask.stdIn, theText);
 }
 
 
@@ -290,11 +593,29 @@ void NTargetSystem::TaskWrite(const TaskInfo &theTask, const NString &theText)
 //      NTargetSystem::TaskWait : Block for a task.
 //----------------------------------------------------------------------------
 void NTargetSystem::TaskWait(const TaskInfo &/*theTask*/, NTime waitFor)
-{
+{	NTime		startTime, endTime, timeLeft;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskWait not implemented!");
+
+	// Get the state we need
+	startTime = NTimeUtilities::GetTime();
+	endTime   = startTime + waitFor;
+
+
+
+	// Wait for the task
+	//
+	// We must allow timers to run while waiting, since they poll the status.
+	//
+	// Despite passing CFRunLoopRunInMode a non-zero time, on 10.6 it will
+	// typically much sooner (forcing us to sleep for the remaining time).
+// dair, CFRunLoop not supported
+NN_LOG("NTargetSystem::TaskWait skipping runloop!");
+//	CFRunLoopRunInMode(kCFRunLoopDefaultMode, waitFor, false);
+
+	timeLeft = endTime - NTimeUtilities::GetTime();
+	if (timeLeft > 0.0)
+		NThread::Sleep(timeLeft);
 }
 
 
@@ -305,11 +626,26 @@ void NTargetSystem::TaskWait(const TaskInfo &/*theTask*/, NTime waitFor)
 //      NTargetSystem::TaskSignal : Signal a task.
 //----------------------------------------------------------------------------
 void NTargetSystem::TaskSignal(const TaskInfo &theTask, NTaskSignal theSignal)
-{
+{	int		sysErr;
 
 
-	// dair, to do
-	NN_LOG("NTargetSystem::TaskSignal not implemented!");
+
+	// Signal the task
+	switch (theSignal) {
+		case kTaskKill:
+			sysErr = kill((pid_t) theTask.taskID, SIGTERM);
+			NN_ASSERT_NOERR(sysErr);
+			break;
+
+		case kTaskInterrupt:
+			sysErr = kill((pid_t) theTask.taskID, SIGINT);
+			NN_ASSERT_NOERR(sysErr);
+			break;
+		
+		default:
+			NN_LOG("Unknown signal: %d", theSignal);
+			break;
+		}
 }
 
 
--- Library/Source/Nano/Internal/Targets/Linux/NLinuxTime.cpp
+++ Library/Source/Nano/Internal/Targets/Linux/NLinuxTime.cpp
@@ -16,6 +16,7 @@
 //----------------------------------------------------------------------------
 #include <sys/time.h>
 
+#include "NLock.h"
 #include "NTargetTime.h"
 
 
@@ -23,8 +24,88 @@
 
 
 //============================================================================
+//		Internal globals
+//----------------------------------------------------------------------------
+static NSpinLock gTimeZoneLock;
+static char		*gTimeZoneOld;
+
+
+
+
+
+//============================================================================
+//		Internal functions
+//----------------------------------------------------------------------------
+//		LockTimeZone : Lock the time zone.
+//----------------------------------------------------------------------------
+static void LockTimeZone(const NString &timeZone)
+{
+
+
+	// Lock the time zone
+	//
+	// Manipulating the environment is the only way to select the time zone used
+	// by POSIX time conversion functions.
+	//
+	// We use a lock to ensure this is 'thread safe', in the sense that Nano will
+	// not clash with itself (unfortunately 3rd party threads may still change this
+	// from under us).
+	gTimeZoneLock.Lock();
+	gTimeZoneOld = getenv("TZ");
+
+	if (timeZone == kNTimeZoneDefault)
+		unsetenv("TZ");
+	else
+		setenv("TZ", timeZone.GetUTF8(), 1);
+
+	tzset();
+
+
+
+	// Check for failure
+	//
+	// Errors can only be detected by checking that a request for a non-UTC timezone
+	// did in fact select a non-UTC timezone.
+	if (timeZone != kNTimeZoneUTC)
+		{
+		NN_ASSERT(strcmp(tzname[0], "UTC") != 0);
+		NN_ASSERT(strcmp(tzname[1], "UTC") != 0);
+		}
+}
+
+
+
+
+
+//============================================================================
+//		UnlockTimeZone : Unlock the time zone.
+//----------------------------------------------------------------------------
+static void UnlockTimeZone(void)
+{
+
+
+	// Unlock the time zone
+	if (gTimeZoneOld != NULL)
+		setenv("TZ", gTimeZoneOld, 1);
+	else
+		unsetenv("TZ");
+	
+	tzset();
+
+	gTimeZoneOld = NULL;
+	gTimeZoneLock.Unlock();
+}
+
+
+
+
+
+//============================================================================
+//		Public functions
+//----------------------------------------------------------------------------
 //		NTargetTime::GetTime : Get the time.
 //----------------------------------------------------------------------------
+#pragma mark -
 NTime NTargetTime::GetTime(void)
 {	struct timeval		timeVal;
 	NTime				theTime;
@@ -121,12 +202,61 @@ void NTargetTime::TimerReset(NTimerID theTimer, NTime fireAfter)
 //		NTargetTime::ConvertTimeToDate : Convert a UTC time to a date.
 //----------------------------------------------------------------------------
 NGregorianDate NTargetTime::ConvertTimeToDate(NTime theTime, const NString &timeZone)
-{	NGregorianDate		theDate = { 0 };
+{	struct tm			zoneTime, queryTime;
+	NTime				intSec, fracSec;
+	time_t				sysTime;
+	NGregorianDate		theDate;
 
 
 
-	// dair, to do
-	NN_LOG("NTargetTime::ConvertTimeToDate not implemented!");
+	// Get the state we need
+	memset(&zoneTime,  0x00, sizeof(zoneTime));
+	memset(&queryTime, 0x00, sizeof(queryTime));
+
+	intSec  = floor(theTime);
+	fracSec = theTime - intSec;
+
+
+
+	// Convert the time
+	LockTimeZone(timeZone);
+
+		// Get the offset
+		//
+		// Converting the UTC time to the equivalent date in the target time zone gives us,
+		// via a Linux/BSD extension to struct tm, the offset of that time at that date in
+		// the target time zone.
+		sysTime = (time_t) (intSec + kNEpochTimeSince1970);
+		localtime_r(&sysTime, &zoneTime);
+
+		queryTime.tm_year  = zoneTime.tm_year;
+		queryTime.tm_mon   = zoneTime.tm_mon;
+		queryTime.tm_mday  = zoneTime.tm_mday;
+		queryTime.tm_hour  = zoneTime.tm_hour;
+		queryTime.tm_min   = zoneTime.tm_min;
+		queryTime.tm_sec   = zoneTime.tm_sec;
+		queryTime.tm_isdst = -1;
+		
+		mktime(&queryTime);
+
+
+		// Convert the time
+		//
+		// We can then adjust the UTC time by that offset to obtain the equivalent date to
+		// that UTC time in the target time zone.
+		sysTime += queryTime.tm_gmtoff;
+		gmtime_r(&sysTime, &zoneTime);
+
+		theDate.year     = zoneTime.tm_year + 1900;
+		theDate.month    = zoneTime.tm_mon  + 1;
+		theDate.day      = zoneTime.tm_mday;
+		theDate.hour     = zoneTime.tm_hour;
+		theDate.minute   = zoneTime.tm_min;
+		theDate.second   = ((NTime) zoneTime.tm_sec) + fracSec;
+		theDate.timeZone = timeZone;
+
+	UnlockTimeZone();
+
 	return(theDate);
 }
 
@@ -138,12 +268,41 @@ NGregorianDate NTargetTime::ConvertTimeToDate(NTime theTime, const NString &time
 //		NTargetTime::ConvertDateToTime : Convert a date to a UTC time.
 //----------------------------------------------------------------------------
 NTime NTargetTime::ConvertDateToTime(const NGregorianDate &theDate)
-{
+{	NTime			intSec, fracSec;
+	struct tm		zoneTime;
+	time_t			sysTime;
+	NTime			theTime;
 
 
-	// dair, to do
-	NN_LOG("NTargetTime::ConvertDateToTime not implemented!");
-	return(0.0);
+
+	// Get the state we need
+	memset(&zoneTime, 0x00, sizeof(zoneTime));
+
+	intSec  = floor(theDate.second);
+	fracSec = theDate.second - intSec;
+
+
+
+	// Convert the date
+	//
+	// mktime gives us the UTC time for the specified date in the target time zone,
+	// which we can then adjust by the required epoch to obtain an NTime.
+	LockTimeZone(theDate.timeZone);
+
+		zoneTime.tm_year  = theDate.year - 1900;
+		zoneTime.tm_mon   = theDate.month;
+		zoneTime.tm_mday  = theDate.day;
+		zoneTime.tm_hour  = theDate.hour;
+		zoneTime.tm_min   = theDate.minute;
+		zoneTime.tm_sec   = (int) intSec;
+		zoneTime.tm_isdst = -1;
+
+		sysTime = mktime(&zoneTime);
+		theTime = -kNEpochTimeSince1970 + sysTime + fracSec;
+
+	UnlockTimeZone();
+
+	return(theTime);
 }
 
 
@@ -154,12 +313,37 @@ NTime NTargetTime::ConvertDateToTime(const NGregorianDate &theDate)
 //		NTargetTime::GetDayOfWeek : Get the day of the week.
 //----------------------------------------------------------------------------
 NIndex NTargetTime::GetDayOfWeek(const NGregorianDate &theDate)
-{
+{	struct tm		zoneTime;
+	NIndex			theIndex;
 
 
-	// dair, to do
-	NN_LOG("NTargetTime::GetDayOfWeek not implemented!");
-	return(0);
+
+	// Get the state we need
+	memset(&zoneTime, 0x00, sizeof(zoneTime));
+
+
+
+	// Get the day of the week
+	//
+	// mktime gives us the UTC time for the specified date in the target time zone.
+	LockTimeZone(theDate.timeZone);
+
+		zoneTime.tm_year  = theDate.year - 1900;
+		zoneTime.tm_mon   = theDate.month;
+		zoneTime.tm_mday  = theDate.day;
+		zoneTime.tm_hour  = theDate.hour;
+		zoneTime.tm_min   = theDate.minute;
+		zoneTime.tm_sec   = (int) theDate.second;
+		zoneTime.tm_isdst = -1;
+
+		mktime(&zoneTime);
+
+		theIndex = zoneTime.tm_wday + 1;
+		NN_ASSERT(theIndex >= 1 && theIndex <= 7);
+
+	UnlockTimeZone();
+
+	return(theIndex);
 }
 
 
