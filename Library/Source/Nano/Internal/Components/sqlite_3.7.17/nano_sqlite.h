363d0d91b3db9e9545af191df87df5b35783f6db
Dair Grant <dair@refnum.com>
2013-06-19 20:15:18 +0100

    Update to SQLite 3.7.17.

---
+++ Library/Source/Nano/Internal/Components/sqlite_3.7.17/nano_sqlite.h
@@ -0,0 +1,26 @@
+/*	NAME:
+		nano_sqlite.h
+
+	DESCRIPTION:
+		SQLite wrapper.
+	
+	COPYRIGHT:
+		Copyright (c) 2006-2013, refNum Software
+		<http://www.refnum.com/>
+
+		All rights reserved. Released under the terms of licence.html.
+	__________________________________________________________________________
+*/
+#ifndef NANO_SQLITE_HDR
+#define NANO_SQLITE_HDR
+//============================================================================
+//		Include files
+//----------------------------------------------------------------------------
+#include "sqlite_3.7.17.h"
+#include "sqlite_3.7.17_fts3_tokenizer.h"
+
+
+
+
+#endif // NANO_SQLITE_HDR
+
