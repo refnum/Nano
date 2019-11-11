f9c93b56d43221d9d1aeb28e6e9f3da026df8ddf
Dair Grant <dair@feralinteractive.com>
2019-11-11 20:58:09 +0000

    Direct NLogger to stderr.

--- Library/Source/Nano/Core/NLogger.cpp
+++ Library/Source/Nano/Core/NLogger.cpp
@@ -2,38 +2,32 @@
 		NLogger.cpp
 
 	DESCRIPTION:
-		Debug log.
+		Logger.
 
 	COPYRIGHT:
 		Copyright (c) 2006-2019, refNum Software
-		<http://www.refnum.com/>
-
 		All rights reserved.
-
-		This software is distributed under the BSD-3-Clause license.
 
 		Redistribution and use in source and binary forms, with or without
 		modification, are permitted provided that the following conditions
 		are met:
-
-			o Redistributions of source code must retain the above
-			copyright notice, this list of conditions and the following
-			disclaimer.
-
-			o Redistributions in binary form must reproduce the above
-			copyright notice, this list of conditions and the following
-			disclaimer in the documentation and/or other materials
-			provided with the distribution.
-
-			o Neither the name of refNum Software nor the names of its
-			contributors may be used to endorse or promote products derived
-			from this software without specific prior written permission.
-
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
 		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-		OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
@@ -46,6 +40,10 @@
 //		Includes
 //-----------------------------------------------------------------------------
 #include "NLogger.h"
+
+// System
+#include <stdio.h>
+#include <string.h>
 
 
 
@@ -63,16 +61,77 @@ NLogger::NLogger()
 
 
 //=============================================================================
+//		NLogger::Log : Log a message.
+//-----------------------------------------------------------------------------
+void NLogger::Log(NLogLevel   logLevel,
+				  const char* fileName,
+				  size_t      lineNum,
+				  const char* theMsg,
+				  va_list     theArgs)
+{
+	// Log the message
+	fprintf(stderr, "%s %s:%d ", GetTokenLevel(logLevel), GetTokenSource(fileName), int(lineNum));
+	vfprintf(stderr, theMsg, theArgs);
+	fprintf(stderr, "\n");
+}
+
+
+
+
+
+//=============================================================================
 //		NLogger::Get : Get the instance.
 //-----------------------------------------------------------------------------
 NLogger* NLogger::Get()
 {
-
-
 	// Get the instance
 	//
 	// Allowed to leak to support logging during static destruction.
 	static NLogger* sInstance = new NLogger;
 
 	return sInstance;
+}
+
+
+
+
+
+//=============================================================================
+//		NLogger::GetTokenLevel : Get a log level token.
+//-----------------------------------------------------------------------------
+const char* NLogger::GetTokenLevel(NLogLevel logLevel) const
+{
+	// Get the token
+	switch (logLevel)
+	{
+		case NLogLevel::Info:
+			return "INFO ";
+			break;
+		case NLogLevel::Warning:
+			return "WARN ";
+			break;
+		case NLogLevel::Error:
+			return "ERR  ";
+			break;
+	}
+}
+
+
+
+
+
+//=============================================================================
+//		NLogger::GetTokenSource : Get a file source token.
+//-----------------------------------------------------------------------------
+const char* NLogger::GetTokenSource(const char* fileName) const
+{
+	// Get the token
+	const char* theToken = strrchr(fileName, '/');
+
+	if (theToken != nullptr)
+	{
+		theToken += 1;
+	}
+
+	return theToken;
 }
--- Library/Source/Nano/Core/NLogger.h
+++ Library/Source/Nano/Core/NLogger.h
@@ -2,38 +2,32 @@
 		NLogger.h
 
 	DESCRIPTION:
-		Debug log.
+		Logger.
 
 	COPYRIGHT:
 		Copyright (c) 2006-2019, refNum Software
-		<http://www.refnum.com/>
-
 		All rights reserved.
-
-		This software is distributed under the BSD-3-Clause license.
 
 		Redistribution and use in source and binary forms, with or without
 		modification, are permitted provided that the following conditions
 		are met:
-
-			o Redistributions of source code must retain the above
-			copyright notice, this list of conditions and the following
-			disclaimer.
-
-			o Redistributions in binary form must reproduce the above
-			copyright notice, this list of conditions and the following
-			disclaimer in the documentation and/or other materials
-			provided with the distribution.
-
-			o Neither the name of refNum Software nor the names of its
-			contributors may be used to endorse or promote products derived
-			from this software without specific prior written permission.
-
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
 		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-		OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
 		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
 		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
@@ -42,11 +36,28 @@
 		OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 	___________________________________________________________________________
 */
-#ifndef NLOGGER_HDR
-#define NLOGGER_HDR
+#ifndef NLOGGER_H
+#define NLOGGER_H
 //=============================================================================
 //		Includes
 //-----------------------------------------------------------------------------
+// System
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+
+
+
+//=============================================================================
+//		Constants
+//-----------------------------------------------------------------------------
+enum class NLogLevel
+{
+	Info,
+	Warning,
+	Error
+};
 
 
 
@@ -58,19 +69,32 @@
 class NLogger
 {
 public:
-	NLogger();
-	~NLogger() = default;
+										NLogger();
+									   ~NLogger() = default;
 
-	NLogger(const NLogger&) = delete;
-	NLogger& operator=(const NLogger&) = delete;
+										NLogger(  const NLogger&) = delete;
+	NLogger&                            operator=(const NLogger&) = delete;
 
-	NLogger(NLogger&&) = delete;
-	NLogger& operator=(NLogger&&) = delete;
+										NLogger(  NLogger&&) = delete;
+	NLogger&                            operator=(NLogger&&) = delete;
+
+
+	// Log a message
+	void                                Log(NLogLevel   logLevel,
+											const char* fileName,
+											size_t      lineNum,
+											const char* theMsg,
+											va_list     theArgs);
 
 
 	// Get the instance
-	static NLogger* Get();
+	static NLogger*                     Get();
+
+
+private:
+	const char*                         GetTokenLevel(NLogLevel logLevel)    const;
+	const char*                         GetTokenSource(const char* fileName) const;
 };
 
 
-#endif // NLOGGER_HDR
+#endif // NLOGGER_H
