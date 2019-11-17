434edd5db0d14089b6c7b3c782c99a73b71c8da8
Dair Grant <dair@feralinteractive.com>
2019-11-17 11:33:22 +0000

    Remove NN_Bxx macros, binary literals now supported by C++14.

--- Library/Source/Nano/Core/NanoMacros.h
+++ Library/Source/Nano/Core/NanoMacros.h
@@ -75,46 +75,6 @@
 #define NN_TEST_BIT(_value, _mask)                          (((_value) & (_mask)) == (_mask))
 
 
-// Declare a binary constant
-//
-//		NN_B8( 01010101)									= 85
-//		NN_B16(10101010, 01010101)							= 43605
-//		NN_B32(10000000, 11111111, 10101010, 01010101)		= 2164238933
-//
-// Based on a PD implementation by Tom Torfs ("Binary constant macros").
-#define _nn_hex(_n)                                         0x##_n##LU
-
-#define _nn_b8(_x)                                                              \
-	(uint8_t)(((_x & 0x0000000FLU) ? 1 : 0) | ((_x & 0x000000F0LU) ? 2 : 0) |   \
-			  ((_x & 0x00000F00LU) ? 4 : 0) | ((_x & 0x0000F000LU) ? 8 : 0) |   \
-			  ((_x & 0x000F0000LU) ? 16 : 0) | ((_x & 0x00F00000LU) ? 32 : 0) | \
-			  ((_x & 0x0F000000LU) ? 64 : 0) | ((_x & 0xF0000000LU) ? 128 : 0))
-
-#define NN_B8(_byte1)                                       ((uint8_t) _nn_b8(_nn_hex(_byte1)))
-
-#define NN_B16(_byte1, _byte2)                              \
-	((((uint16_t) NN_B8(_byte1)) << 8) | (((uint16_t) NN_B8(_byte2)) << 0))
-
-#define NN_B32(_byte1, _byte2, _byte3, _byte4)                                  \
-	((((uint32_t) NN_B8(_byte1)) << 24) | (((uint32_t) NN_B8(_byte2)) << 16) |  \
-	 (((uint32_t) NN_B8(_byte3)) << 8) | (((uint32_t) NN_B8(_byte4)) << 0))
-
-
-// Format validation
-//
-// Example:
-//
-//		NN_VALIDATE_PRINTF(1, 2)
-//		void Print(const char* theMsg, ...);
-//
-#if NN_COMPILER_CLANG || NN_COMPILAER_GCC
-	#define NN_VALIDATE_PRINTF(_formatIndex, _firstParamIndex)  \
-		__attribute__((format(printf, _formatIndex, _firstParamIndex)))
-#else
-	#define NN_VALIDATE_PRINTF(_formatIndex, _firstParamIndex)
-#endif
-
-
 // Indicate an expression is likely
 //
 // Example:
