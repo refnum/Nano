1efa27fc4e2a20e02f8cba0c86b7d672bbc93d95
Dair Grant <dair@feralinteractive.com>
2019-11-21 22:18:32 +0000

    Switch TDataDigest over to NANO_FIXURE and NANO_TEST.

--- NanoTest/Source/Nano/System/TDataDigest.cpp
+++ NanoTest/Source/Nano/System/TDataDigest.cpp
@@ -5,42 +5,66 @@
 		NDataDigest tests.
 
 	COPYRIGHT:
-		Copyright (c) 2006-2013, refNum Software
-		<http://www.refnum.com/>
+		Copyright (c) 2006-2019, refNum Software
+		All rights reserved.
 
-		All rights reserved. Released under the terms of licence.html.
-	__________________________________________________________________________
+		Redistribution and use in source and binary forms, with or without
+		modification, are permitted provided that the following conditions
+		are met:
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+		(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+		OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	___________________________________________________________________________
 */
-//============================================================================
-//		Include files
-//----------------------------------------------------------------------------
-#include "NTestFixture.h"
+//=============================================================================
+//		Includes
+//-----------------------------------------------------------------------------
+// Nano
 #include "NDataDigest.h"
+#include "NTestFixture.h"
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		Constants
-//----------------------------------------------------------------------------
-static const uint8_t kTestData1[]									= { 0xAA, 0xBB, 0xCC, 0xDD };
-static const uint8_t kTestData2[]									= { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 };
+//-----------------------------------------------------------------------------
+static const uint8_t kTestData1[]                           = {0xAA, 0xBB, 0xCC, 0xDD};
+static const uint8_t kTestData2[]                           = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		Test fixture
-//----------------------------------------------------------------------------
-#define TEST_NDATADIGEST(...)										TEST_NANO(TDataDigest, ##__VA_ARGS__)
-
-FIXTURE_NANO(TDataDigest)
+//-----------------------------------------------------------------------------
+NANO_FIXTURE(TDataDigest)
 {
-	NData			data1, data2, data3;
-	NDataDigest		theDigest;
-	
+	NData       data1, data2, data3;
+	NDataDigest theDigest;
+
 	SETUP
 	{
 		data1 = NData(NN_ARRAY_SIZE(kTestData1), kTestData1);
@@ -52,10 +76,10 @@ FIXTURE_NANO(TDataDigest)
 
 
 
-//============================================================================
+//=============================================================================
 //		Test case
-//----------------------------------------------------------------------------
-TEST_NDATADIGEST("Internet")
+//-----------------------------------------------------------------------------
+NANO_TEST(TDataDigest, "Internet")
 {
 
 
@@ -69,10 +93,10 @@ TEST_NDATADIGEST("Internet")
 
 
 
-//============================================================================
+//=============================================================================
 //		Test case
-//----------------------------------------------------------------------------
-TEST_NDATADIGEST("DJB2")
+//-----------------------------------------------------------------------------
+NANO_TEST(TDataDigest, "DJB2")
 {
 
 
@@ -86,10 +110,10 @@ TEST_NDATADIGEST("DJB2")
 
 
 
-//============================================================================
+//=============================================================================
 //		Test case
-//----------------------------------------------------------------------------
-TEST_NDATADIGEST("Adler32")
+//-----------------------------------------------------------------------------
+NANO_TEST(TDataDigest, "Adler32")
 {
 
 
@@ -103,10 +127,10 @@ TEST_NDATADIGEST("Adler32")
 
 
 
-//============================================================================
+//=============================================================================
 //		Test case
-//----------------------------------------------------------------------------
-TEST_NDATADIGEST("MD5")
+//-----------------------------------------------------------------------------
+NANO_TEST(TDataDigest, "MD5")
 {
 
 
@@ -120,17 +144,18 @@ TEST_NDATADIGEST("MD5")
 
 
 
-//============================================================================
+//=============================================================================
 //		Test case
-//----------------------------------------------------------------------------
-TEST_NDATADIGEST("SHA1")
+//-----------------------------------------------------------------------------
+NANO_TEST(TDataDigest, "SHA1")
 {
 
 
 	// Perform the test
-	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data1)) == "a7b7e9592daa0896db0517bf8ad53e56b1246923");
-	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data2)) == "868460d98d09d8bbb93d7b6cdd15cc7fbec676b9");
-	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data3)) == "0000000000000000000000000000000000000000");
+	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data1)) ==
+			"a7b7e9592daa0896db0517bf8ad53e56b1246923");
+	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data2)) ==
+			"868460d98d09d8bbb93d7b6cdd15cc7fbec676b9");
+	REQUIRE(theDigest.GetString(theDigest.GetSHA1(data3)) ==
+			"0000000000000000000000000000000000000000");
 }
-
-
