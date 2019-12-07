47b7bb5a787944fe4e805a8ffa0d68b6a18b94e7
Dair Grant <dair@refnum.com>
2019-12-07 20:17:46 +0000

    Add modern CMake-style cmakelists.

---
+++ Library/Support/CMake/NanoConfiguration.cmake
@@ -0,0 +1,144 @@
+#==============================================================================
+#		Configuration
+#------------------------------------------------------------------------------
+set(NN_DEBUG												0)
+set(NN_RELEASE												0)
+
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+	set(NN_DEBUG											1)
+
+elseif (CMAKE_BUILD_TYPE STREQUAL "Release")
+	set(NN_RELEASE											1)
+
+else()
+	message(FATAL_ERROR "[Nano] Unable to identify configuration from '${CMAKE_BUILD_TYPE}'")
+endif()
+
+
+
+
+
+#==============================================================================
+#		Compiler - Configuration
+#------------------------------------------------------------------------------
+set(        NN_COMPILER_FLAGS "")
+list(APPEND NN_COMPILER_FLAGS -DNN_DEBUG=0)
+list(APPEND NN_COMPILER_FLAGS -DNN_RELEASE=0)
+
+
+if (NN_DEBUG)
+	list(APPEND NN_COMPILER_FLAGS -UNN_DEBUG -DNN_DEBUG=1)
+else()
+	list(APPEND NN_COMPILER_FLAGS -UNN_RELEASE -DRELEASE=1)
+endif()
+
+
+if (NN_COMPILER_CLANG)
+	list(APPEND NN_COMPILER_FLAGS -fconstant-cfstrings)
+	list(APPEND NN_COMPILER_FLAGS -fno-common)
+
+	if (NN_DEBUG)
+		list(APPEND NN_COMPILER_FLAGS -O0)
+		list(APPEND NN_COMPILER_FLAGS -fstack-protector-all)
+		list(APPEND NN_COMPILER_FLAGS -D_FORTIFY_SOURCE=2)
+	else()
+		list(APPEND NN_COMPILER_FLAGS -O3)
+		list(APPEND NN_COMPILER_FLAGS -fno-stack-protector)
+		list(APPEND NN_COMPILER_FLAGS -DNN_DEBUG=0)
+	endif()
+
+elseif (NN_COMPILER_GCC)
+
+	if (NN_DEBUG)
+		list(APPEND NN_COMPILER_FLAGS -O0)
+	else()
+		list(APPEND NN_COMPILER_FLAGS -O3)
+	endif()
+
+endif()
+
+
+
+
+
+#==============================================================================
+#		Compiler - Warnings
+#------------------------------------------------------------------------------
+set(NN_COMPILER_WARNINGS "")
+
+if (NN_COMPILER_CLANG)
+	# Maximum warnings
+	list(APPEND NN_COMPILER_WARNINGS -Weverything)
+	list(APPEND NN_COMPILER_WARNINGS -Werror)
+
+	# Revert Xcode suppressions)
+	list(APPEND NN_COMPILER_WARNINGS -Wblock-capture-autoreleasing)
+	list(APPEND NN_COMPILER_WARNINGS -Wbool-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wcomma)
+	list(APPEND NN_COMPILER_WARNINGS -Wconstant-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wconversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wempty-body)
+	list(APPEND NN_COMPILER_WARNINGS -Wenum-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wexit-time-destructors)
+	list(APPEND NN_COMPILER_WARNINGS -Wfloat-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wfour-char-constants)
+	list(APPEND NN_COMPILER_WARNINGS -Winfinite-recursion)
+	list(APPEND NN_COMPILER_WARNINGS -Wint-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wmissing-braces)
+	list(APPEND NN_COMPILER_WARNINGS -Wmissing-field-initializers)
+	list(APPEND NN_COMPILER_WARNINGS -Wmissing-prototypes)
+	list(APPEND NN_COMPILER_WARNINGS -Wmove)
+	list(APPEND NN_COMPILER_WARNINGS -Wnewline-eof)
+	list(APPEND NN_COMPILER_WARNINGS -Wnon-literal-null-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wnon-virtual-dtor)
+	list(APPEND NN_COMPILER_WARNINGS -Wobjc-literal-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Woverloaded-virtual)
+	list(APPEND NN_COMPILER_WARNINGS -Wrange-loop-analysis)
+	list(APPEND NN_COMPILER_WARNINGS -Wreturn-type)
+	list(APPEND NN_COMPILER_WARNINGS -Wsemicolon-before-method-body)
+	list(APPEND NN_COMPILER_WARNINGS -Wshadow)
+	list(APPEND NN_COMPILER_WARNINGS -Wshorten-64-to-32)
+	list(APPEND NN_COMPILER_WARNINGS -Wsign-conversion)
+	list(APPEND NN_COMPILER_WARNINGS -Wstrict-prototypes)
+	list(APPEND NN_COMPILER_WARNINGS -Wtrigraphs)
+	list(APPEND NN_COMPILER_WARNINGS -Wuninitialized)
+	list(APPEND NN_COMPILER_WARNINGS -Wunknown-pragmas)
+	list(APPEND NN_COMPILER_WARNINGS -Wunused-function)
+	list(APPEND NN_COMPILER_WARNINGS -Wunused-label)
+	list(APPEND NN_COMPILER_WARNINGS -Wunused-parameter)
+	list(APPEND NN_COMPILER_WARNINGS -Wunused-variable)
+
+	# Discard unwanted warnings
+	list(APPEND NN_COMPILER_WARNINGS -Wno-atomic-implicit-seq-cst)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-c++17-extensions)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-c++98-compat)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-c++98-compat-pedantic)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-c99-extensions)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-documentation-unknown-command)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-exit-time-destructors)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-float-equal)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-format-nonliteral)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-global-constructors)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-gnu-zero-variadic-macro-arguments)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-nested-anon-types)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-packed)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-padded)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-return-std-move-in-c++11)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-switch-enum)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-unreachable-code-break)
+	list(APPEND NN_COMPILER_WARNINGS -Wno-weak-vtables)
+
+elseif (NN_COMPILER_GCC)
+	# Maximum warnings
+	list(APPEND NN_COMPILER_WARNINGS -Wall)
+	list(APPEND NN_COMPILER_WARNINGS -Wextra)
+	list(APPEND NN_COMPILER_WARNINGS -Werror)
+
+elseif (NN_COMPILER_MSC)
+
+endif()
+
+
+
+
+
---
+++ Library/Support/CMake/NanoFunctions.cmake
@@ -0,0 +1,109 @@
+#==============================================================================
+#		target_nano_create_groups : Organise source into groups for IDEs.
+#------------------------------------------------------------------------------
+function(target_nano_create_groups target)
+
+	get_target_property(theSources "${target}" SOURCES)
+
+	foreach (theFile ${theSources})
+		get_filename_component(thePath "${theFile}" ABSOLUTE)
+		get_filename_component(thePath "${thePath}" PATH)
+
+		if ("${theFile}" MATCHES "${PROJECT_BINARY_DIR}")
+			file(RELATIVE_PATH "${thePath}" "${PROJECT_BINARY_DIR}" "${thePath}")
+		else()
+			file(RELATIVE_PATH "${thePath}" "${PROJECT_SOURCE_DIR}" "${thePath}")
+		endif()
+
+		string(REGEX REPLACE "/" "\\\\" winPath "${thePath}")
+		source_group("${winPath}" REGULAR_EXPRESSION "${thePath}/[^/\\]+\\..*")
+	endforeach()
+
+endfunction()
+
+
+
+
+
+#==============================================================================
+#		target_nano_compile_options : Set the compiler options.
+#------------------------------------------------------------------------------
+function(target_nano_compile_options theTarget)
+
+	# Features
+	target_compile_features("${theTarget}" PUBLIC "cxx_std_17")
+	target_compile_features("${theTarget}" PUBLIC "c_std_11")
+
+
+	# Flags
+	target_compile_options("${theTarget}" PRIVATE ${NN_COMPILER_FLAGS})
+	target_compile_options("${theTarget}" PRIVATE ${NN_COMPILER_WARNINGS})
+
+endfunction()
+
+
+
+
+
+
+#==============================================================================
+#		target_nano_find_link_library : Find and link a library.
+#------------------------------------------------------------------------------
+function(target_nano_find_link_library theTarget theLibrary)
+
+	find_library("NN_LIBRARY_${theLibrary}" "${theLibrary}")
+	target_link_libraries("${theTarget}" PRIVATE "${NN_LIBRARY_${theLibrary}}")
+
+endfunction()
+
+
+
+
+
+#==============================================================================
+#		target_nano_build_library : Build a library target.
+#------------------------------------------------------------------------------
+function(target_nano_build_library theTarget)
+
+	target_nano_compile_options("${theTarget}")
+	target_nano_create_groups(  "${theTarget}")
+
+endfunction()
+
+
+
+
+
+#==============================================================================
+#		nano_target_build_app : Build the current project as an app.
+#------------------------------------------------------------------------------
+function(nano_target_build_app theTarget)
+
+	target_nano_compile_options("${theTarget}")
+	target_nano_create_groups(	"${theTarget}")
+	target_link_libraries(		"${theTarget}" PRIVATE "Nano")
+
+
+	if (NN_TARGET_ANDROID)
+		target_link_libraries("${theTarget}" PRIVATE "android")
+		target_link_libraries("${theTarget}" PRIVATE "log")
+
+	elseif (NN_TARGET_IOS)
+		target_nano_find_link_library("${theTarget}" "CoreFoundation")
+
+	elseif (NN_TARGET_LINUX)
+
+	elseif (NN_TARGET_MACOS)
+		target_nano_find_link_library("${theTarget}" "CoreFoundation")
+
+	elseif (NN_TARGET_TVOS)
+		target_nano_find_link_library("${theTarget}" "CoreFoundation")
+
+	elseif (NN_TARGET_WINDOWS)
+
+	endif()
+
+endfunction()
+
+
+
