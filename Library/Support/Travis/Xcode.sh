bb9d152801519dcca3f7ebd39b259f6a23e42021
Dair Grant <dair@refnum.com>
2019-12-07 21:32:51 +0000

    Switch to bash for Travis scripts.

--- Library/Support/Travis/Xcode.sh
+++ Library/Support/Travis/Xcode.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 
 
 # Get the arguments
@@ -7,11 +8,16 @@ TRAVIS_PLATFORM="$2"
 
 
 
+# Prepare to build
+set -o pipefail
+
+
+
 # Parse the project
-if [ "${TRAVIS_PROJECT}" = "Nano" ]; then
+if [ "${TRAVIS_PROJECT}" == "Nano" ]; then
 	XCODE_PROJECT="Library/Project/Nano.xcodeproj"
 
-elif [ "${TRAVIS_PROJECT}" = "NanoTest" ]; then
+elif [ "${TRAVIS_PROJECT}" == "NanoTest" ]; then
 	XCODE_PROJECT="NanoTest/Project/NanoTest.xcodeproj"
 
 else
@@ -22,13 +28,13 @@ fi
 
 
 # Parse the platform
-if [ "${TRAVIS_PLATFORM}" = "macOS" ]; then
+if [ "${TRAVIS_PLATFORM}" == "macOS" ]; then
 	XCODE_DESTINATION="arch=x86_64"
 
-elif [ "${TRAVIS_PLATFORM}" = "iOS" ]; then
+elif [ "${TRAVIS_PLATFORM}" == "iOS" ]; then
 	XCODE_DESTINATION="generic/platform=iOS"
 
-elif [ "${TRAVIS_PLATFORM}" = "tvOS" ]; then
+elif [ "${TRAVIS_PLATFORM}" == "tvOS" ]; then
 	XCODE_DESTINATION="generic/platform=tvOS"
 
 else
@@ -54,7 +60,7 @@ done
 
 
 # Run the tests
-if [ "${TRAVIS_PROJECT}" = "NanoTest" ]; then
+if [ "${TRAVIS_PROJECT}" == "NanoTest" ]; then
 	find . -name "NanoTest"
 fi
 
