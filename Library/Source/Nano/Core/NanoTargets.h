bfc29abfc8026533c0f0d6ef3dec3bbc33f1822c
Dair Grant <dair@refnum.com>
2019-12-07 22:08:57 +0000

    Re-apply rn-format.

--- Library/Source/Nano/Core/NanoTargets.h
+++ Library/Source/Nano/Core/NanoTargets.h
@@ -82,7 +82,7 @@
 
 // macOS
 #if (defined(__APPLE_CPP__) || defined(__APPLE_CC__)) && !defined(__arm__) &&   \
-	!defined(__ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__) &&  \
+	!defined(__ENVIRONMENT_TV_OS_VERSION_MIN_REQUIRED__) &&                     \
 	!defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
 	#undef  NN_TARGET_MACOS
 	#define NN_TARGET_MACOS                                 1
