5bea6cc0aec78eb70685778395d5f93ca616b9e9
Dair Grant <dair@refnum.com>
2019-12-07 01:36:51 +0000

    Suppress -Wpragmas for gcc to suppress -Wused-but-marked-unused.

--- Library/Source/Nano/Internal/Components/xxHash_0.7.2/Nano_xxHash.h
+++ Library/Source/Nano/Internal/Components/xxHash_0.7.2/Nano_xxHash.h
@@ -53,6 +53,7 @@
 NN_DIAGNOSTIC_PUSH()
 NN_DIAGNOSTIC_IGNORE("-Wimplicit-fallthrough")
 NN_DIAGNOSTIC_IGNORE("-Wold-style-cast")
+NN_DIAGNOSTIC_IGNORE("-Wpragmas")
 NN_DIAGNOSTIC_IGNORE("-Wunknown-pragmas")
 NN_DIAGNOSTIC_IGNORE("-Wzero-as-null-pointer-constant")
 
