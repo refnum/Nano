64ba0fc67f5387d42fb7a0ed9f3e741f47808e64
Dair Grant <dair@refnum.com>
2019-12-09 21:42:22 +0000

    Testing MSVC macro expansion.

--- Library/Source/Nano/Core/NDebug.h
+++ Library/Source/Nano/Core/NDebug.h
@@ -319,7 +319,7 @@
 #define NN_REQUIRE_NOT_NULL(_pointer, ...)                  \
 	do                                                      \
 	{                                                       \
-		NN_EXPAND(NN_REQUIRE((_pointer) != nullptr, ##__VA_ARGS__));   \
+		NN_REQUIRE((_pointer) != nullptr, ##__VA_ARGS__);   \
 		NN_UNUSED(_pointer);                                \
 	} while (false)
 
