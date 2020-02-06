194e6134b97a74d339027c5e36f5e529ac31fa44
Dair Grant <dair@refnum.com>
2020-02-06 21:45:48 +0000

    Add additional NString tests.

--- NanoTest/Source/Nano/Text/TString.cpp
+++ NanoTest/Source/Nano/Text/TString.cpp
@@ -40,6 +40,7 @@
 //		Includes
 //-----------------------------------------------------------------------------
 // Nano
+#include "NData.h"
 #include "NString.h"
 #include "NTestFixture.h"
 
@@ -50,7 +51,24 @@
 //=============================================================================
 //		Internal Constants
 //-----------------------------------------------------------------------------
-static const NString kTestString                            = "another test string";
+static const uint8_t kTestDataSmallUTF8[] =
+	{0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64};
+
+static const uint16_t kTestDataLargeUTF16[]                 = {
+	0x0048, 0x0065, 0x006C, 0x006C, 0x006F, 0x0020, 0x0057, 0x006F, 0x0072, 0x006C, 0x0064,
+	0x0020, 0x0054, 0x0065, 0x0073, 0x0074, 0x0069, 0x006E, 0x0067, 0x0020, 0x0054, 0x0065,
+	0x0073, 0x0074, 0x0069, 0x006E, 0x0067, 0x0020, 0x004F, 0x006E, 0x0065, 0x0020, 0x0054,
+	0x0077, 0x006F, 0x0020, 0x0054, 0x0068, 0x0072, 0x0065, 0x0065};
+
+static const uint32_t kTestDataLargeUTF32[]                 = {
+	0x000048, 0x000065, 0x00006C, 0x00006C, 0x00006F, 0x000020, 0x000057, 0x00006F, 0x000072,
+	0x00006C, 0x000064, 0x000020, 0x000054, 0x000065, 0x000073, 0x000074, 0x000069, 0x00006E,
+	0x000067, 0x000020, 0x000054, 0x000065, 0x000073, 0x000074, 0x000069, 0x00006E, 0x000067,
+	0x000020, 0x00004F, 0x00006E, 0x000065, 0x000020, 0x000054, 0x000077, 0x00006F, 0x000020,
+	0x000054, 0x000068, 0x000072, 0x000065, 0x000065};
+
+static const NString kTestStringSmall                       = "Hello World";
+static const NString kTestStringLarge                       = "Hello World Testing Testing One Two Three";
 
 
 
@@ -61,7 +79,13 @@ static const NString kTestString                            = "another test stri
 //-----------------------------------------------------------------------------
 NANO_FIXTURE(TString)
 {
-	NString theString;
+	std::vector<NString> stringObjects;
+
+	SETUP
+	{
+		stringObjects.push_back(kTestStringSmall);
+		stringObjects.push_back(kTestStringLarge);
+	}
 };
 
 
@@ -76,8 +100,50 @@ NANO_TEST(TString, "Default")
 
 
 	// Perform the test
+	NString theString;
+
 	REQUIRE(theString.IsEmpty());
+	REQUIRE(theString.GetHash() == 0);
 	REQUIRE(theString.GetSize() == 0);
+	REQUIRE(theString.GetUTF8() != nullptr);
+	REQUIRE(strcmp(theString.GetUTF8(), "") == 0);
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "Container")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(!theString.IsEmpty());
+		REQUIRE(theString.GetSize() != 0);
+	}
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "Hashable")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(theString.GetHash() != 0);
+	}
 }
 
 
@@ -92,21 +158,78 @@ NANO_TEST(TString, "Construction")
 
 
 	// Perform the test
-	NString stringOne("String Literal");
-	NN_REQUIRE(stringOne.GetSize() == 14);
+	NString theString;
+
+	theString = NString(u8"UTF8 Small");
+	REQUIRE(theString.GetSize() == 10);
+
+	theString = NString(u8"UTF8 Large Large Large Large Large Large Large");
+	REQUIRE(theString.GetSize() == 46);
+
+	theString = NString(u"UTF16 Small");
+	REQUIRE(theString.GetSize() == 11);
+
+	theString = NString(u"UTF16 Large Large Large Large Large Large Large");
+	REQUIRE(theString.GetSize() == 47);
+
+	theString = NString(U"UTF32 Small");
+	REQUIRE(theString.GetSize() == 11);
+
+	theString = NString(U"UTF32 Large Large Large Large Large Large Large");
+	REQUIRE(theString.GetSize() == 47);
+
+	theString = NString(NStringEncoding::UTF8, sizeof(kTestDataSmallUTF8), kTestDataSmallUTF8);
+	REQUIRE(theString.GetSize() == 11);
+
+	theString = NString(NStringEncoding::UTF16, sizeof(kTestDataLargeUTF16), kTestDataLargeUTF16);
+	REQUIRE(theString.GetSize() == 41);
+
+	theString = NString(NStringEncoding::UTF32, sizeof(kTestDataLargeUTF32), kTestDataLargeUTF32);
+	REQUIRE(theString.GetSize() == 41);
+
+	theString = NString();
+	REQUIRE(theString.GetSize() == 0);
+
+
+	theString = NString("String Literal");
+	REQUIRE(theString.GetSize() == 14);
 
 	const char* localPtr = "Char Pointer";
-	NString     stringTwo(localPtr);
-	NN_REQUIRE(stringTwo.GetSize() == 12);
+	theString            = NString(localPtr);
+	REQUIRE(theString.GetSize() == 12);
 
-	char localArray[20];
-	strncpy(localArray, "Local Array", std::size(localArray));
-	NString stringThree(localArray);
-	NN_REQUIRE(stringThree.GetSize() == 11);
+	char charArray[20];
+	strncpy(charArray, "Char Array", std::size(charArray));
+	theString = NString(charArray);
+	REQUIRE(theString.GetSize() == 10);
 
-	std::string localString("Std String");
-	NString     stringFour(localString.c_str());
-	NN_REQUIRE(stringFour.GetSize() == 10);
+	std::string stdString("Std String");
+	theString = NString(stdString.c_str());
+	REQUIRE(theString.GetSize() == 10);
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "Clear")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(!theString.IsEmpty());
+		REQUIRE(theString.GetSize() != 0);
+
+		theString.Clear();
+
+		REQUIRE(theString.IsEmpty());
+		REQUIRE(theString.GetSize() == 0);
+	}
 }
 
 
@@ -121,13 +244,93 @@ NANO_TEST(TString, "GetText")
 
 
 	// Perform the test
-	theString = kTestString;
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(theString.GetText(NStringEncoding::UTF8) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::UTF16) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::UTF32) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::ASCII) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::MacRoman) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::ISOLatin1) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::WindowsLatin1) != nullptr);
+	}
+}
 
-	REQUIRE(theString.GetText(NStringEncoding::UTF8) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::UTF16) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::UTF32) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::ASCII) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::MacRoman) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::ISOLatin1) != nullptr);
-	REQUIRE(theString.GetText(NStringEncoding::WindowsLatin1) != nullptr);
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "GetUTF")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(theString.GetUTF8() != nullptr);
+		REQUIRE(theString.GetUTF16() != nullptr);
+		REQUIRE(theString.GetUTF32() != nullptr);
+
+		REQUIRE(theString.GetText(NStringEncoding::UTF8) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::UTF16) != nullptr);
+		REQUIRE(theString.GetText(NStringEncoding::UTF32) != nullptr);
+	}
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "GetData")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		REQUIRE(!theString.GetData(NStringEncoding::UTF8).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::UTF16).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::UTF32).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::ASCII).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::MacRoman).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::ISOLatin1).IsEmpty());
+		REQUIRE(!theString.GetData(NStringEncoding::WindowsLatin1).IsEmpty());
+	}
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TString, "SetData")
+{
+
+
+	// Perform the test
+	for (auto theString : stringObjects)
+	{
+		NString tmpString;
+		NData   tmpData;
+
+		tmpData = theString.GetData(NStringEncoding::UTF8);
+		tmpString.SetData(NStringEncoding::UTF8, tmpData);
+		REQUIRE(tmpString.GetData(NStringEncoding::UTF8) == tmpData);
+
+		tmpData = theString.GetData(NStringEncoding::UTF16);
+		tmpString.SetData(NStringEncoding::UTF16, tmpData);
+		REQUIRE(tmpString.GetData(NStringEncoding::UTF16) == tmpData);
+
+		tmpData = theString.GetData(NStringEncoding::UTF32);
+		tmpString.SetData(NStringEncoding::UTF32, tmpData);
+		REQUIRE(tmpString.GetData(NStringEncoding::UTF32) == tmpData);
+	}
 }
