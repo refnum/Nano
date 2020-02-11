a37396b88d63e1be672aa7c5fae3ba131436026e
Dair Grant <dair@refnum.com>
2020-02-11 22:50:56 +0000

    Windows debugging.

--- Library/Source/Nano/Text/NString.cpp
+++ Library/Source/Nano/Text/NString.cpp
@@ -621,6 +621,7 @@ bool NString::StartsWith(const NString& theString, NStringFlags theFlags) const
 	//
 	// By using a pattern search we can anchor the search term at the start.
 NStringFlags tmp = theFlags;
+ tmp = kNStringPattern;
  tmp = theFlags | kNStringPattern;
 NN_UNUSED(tmp);
 	return !Find("\\A" + theString, theFlags | kNStringPattern).IsEmpty();
