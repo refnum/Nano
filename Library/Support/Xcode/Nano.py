154bb9cc9d1b1e64120f467a58aa01d90c7b9b5b
Dair Grant <dair@refnum.com>
2020-02-27 20:20:30 +0000

    Add lldb formatters for NRange, NData, and NString.

--- Library/Support/Xcode/Nano.py
+++ Library/Support/Xcode/Nano.py
@@ -11,7 +11,7 @@
 #			command script import /path/to/Nano.py
 #
 #	COPYRIGHT:
-#		Copyright (c) 2006-2019, refNum Software
+#		Copyright (c) 2006-2020, refNum Software
 #		All rights reserved.
 #
 #		Redistribution and use in source and binary forms, with or without
@@ -43,37 +43,307 @@
 #==============================================================================
 #		Imports
 #------------------------------------------------------------------------------
+import struct
+import array
 import lldb
-import os
 import sys
+import os
 
 
 
 
 
-#=============================================================================
-#		SummariseNData : Summarise an NData.
-#-----------------------------------------------------------------------------
-def SummariseNData(theValue, theInfo):
+#==============================================================================
+#		Constants
+#------------------------------------------------------------------------------
+# Constants
+kNNotFound													= 18446744073709551615;
 
-	# Get the size
-	kSmallSizeBit   = 0b00000001
-	kSmallSizeMask  = 0b11111000
-	kSmallSizeShift = 3
+kNDataFlagIsLarge											= 0b10000000;
+kNDataFlagSmallSizeMask										= 0b00011111;
 
-	sizeFlags = theValue.GetValueForExpressionPath("->mSmall.sizeFlags").GetValueAsUnsigned(0)
+kNStringFlagIsLarge											= 0b10000000;
+kNStringFlagIsSmallUTF16									= 0b01000000;
+kNStringFlagSmallSizeMask									= 0b00011111;
 
-	if (sizeFlags & kSmallSizeBit):
-		theSize = (sizeFlags & kSmallSizeMask) >> kSmallSizeShift;
+NStringEncoding_Unknown										= 0;
+NStringEncoding_UTF8										= 1;
+NStringEncoding_UTF16										= 2;
+NStringEncoding_UTF32										= 3;
+NStringEncoding_UTF16BE										= 4;
+NStringEncoding_UTF16LE										= 5;
+NStringEncoding_UTF32BE										= 6;
+NStringEncoding_UTF32LE										= 7;
+NStringEncoding_ASCII										= 8;
+NStringEncoding_MacRoman									= 9;
+NStringEncoding_ISOLatin1									= 10;
+NStringEncoding_WindowsLatin1								= 11;
+
+NStringEncodings = {
+	NStringEncoding_Unknown			: { "size" : 1, "name" : "undefined"	},
+	NStringEncoding_UTF8			: { "size" : 1, "name" : "utf_8"		},
+	NStringEncoding_UTF16			: { "size" : 2, "name" : "utf_16"		},
+	NStringEncoding_UTF32			: { "size" : 4, "name" : "utf_32"		},
+	NStringEncoding_UTF16BE			: { "size" : 2, "name" : "utf_16_be"	},
+	NStringEncoding_UTF16LE			: { "size" : 2, "name" : "utf_16_le"	},
+	NStringEncoding_UTF32BE			: { "size" : 4, "name" : "utf_32_be"	},
+	NStringEncoding_UTF32LE			: { "size" : 4, "name" : "utf_32_le"	},
+	NStringEncoding_ASCII			: { "size" : 1, "name" : "ascii"		},
+	NStringEncoding_MacRoman		: { "size" : 1, "name" : "mac_roman"	},
+	NStringEncoding_ISOLatin1		: { "size" : 1, "name" : "latin_1"		},
+	NStringEncoding_WindowsLatin1	: { "size" : 1, "name" : "cp1252"		}
+}
+
+
+
+
+
+#==============================================================================
+#		getMemberUInt : Get member as an unsigned integer.
+#------------------------------------------------------------------------------
+def getMemberUInt(theValue, theName):
+
+	return theValue.GetChildMemberWithName(theName).GetValueAsUnsigned(0)
+
+
+
+
+
+#==============================================================================
+#		getPathValue : Get a path as an SBValue.
+#------------------------------------------------------------------------------
+def getPathValue(theValue, thePath):
+
+	return theValue.GetValueForExpressionPath(thePath)
+
+
+
+
+
+#==============================================================================
+#		getPathUInt : Get a path as an unsigned integer.
+#------------------------------------------------------------------------------
+def getPathUInt(theValue, thePath):
+
+	return theValue.GetValueForExpressionPath(thePath).GetValueAsUnsigned(0)
+
+
+
+
+
+#==============================================================================
+#		getPathData : Get a path as an SBData.
+#------------------------------------------------------------------------------
+def getPathData(theValue, thePath):
+
+	return theValue.GetValueForExpressionPath(thePath).GetData()
+
+
+
+
+
+#==============================================================================
+#		getMemory : Get bytes from an address.
+#------------------------------------------------------------------------------
+def getMemory(thePtr, theOffset, theSize):
+
+	theErr     = lldb.SBError()
+	theProcess = thePtr.GetProcess()
+
+	theAddresss = thePtr.GetValueAsUnsigned() + theOffset
+	theBytes    = theProcess.ReadMemory(theAddresss, theSize, theErr)
+
+	return theBytes
+
+
+
+
+
+#==============================================================================
+#		getEncodingSize : Get the code unit size of an encoding.
+#------------------------------------------------------------------------------
+def getEncodingSize(theEncoding):
+
+	return NStringEncodings.get(theEncoding).get("size");
+
+
+
+
+
+#==============================================================================
+#		getEncodingName : Get the Python name of an encoding.
+#------------------------------------------------------------------------------
+def getEncodingName(theEncoding):
+
+	return NStringEncodings.get(theEncoding).get("name");
+
+
+
+
+
+#==============================================================================
+#		getText : Convert bytes to text.
+#------------------------------------------------------------------------------
+def getText(theBytes, theEncoding):
+
+	pyEncoding = getEncodingName(theEncoding);
+	theText    = theBytes.decode(pyEncoding)
+	
+	return theText
+
+
+
+
+
+#==============================================================================
+#		SBData_GetBytes : Get bytes from an SBData.
+#------------------------------------------------------------------------------
+def SBData_GetBytes(sbData, theOffset, theSize):
+
+	sbErr    = lldb.SBError()
+	theBytes = sbData.ReadRawData(sbErr, theOffset, theSize)
+
+	return theBytes
+
+
+
+
+
+#==============================================================================
+#		NData_GetBytes : Get the bytes from an NData.
+#------------------------------------------------------------------------------
+def NData_GetBytes(theData):
+
+	theFlags = getPathUInt(theData, "->mData.theFlags")
+	isLarge  = (theFlags & kNDataFlagIsLarge)
+
+	if (isLarge):
+		theOffset = getPathUInt(theData, "->mData.Large.theSlice.mLocation")
+		theSize   = getPathUInt(theData, "->mData.Large.theSlice.mSize")
+		dataPtr   = getPathValue(theData, "->mData.Large.theState->theData")
+		theBytes  = getMemory(dataPtr, theOffset, theSize)
+
 	else:
-		theSize = theValue.GetValueForExpressionPath("->mShared.theSlice.mSize").GetValueAsUnsigned(0)
+		theSize  = (theFlags & kNDataFlagSmallSizeMask)
+		theBytes = getPathData(theData, "->mData.Small.theData").uint8s
+
+	return theBytes
 
 
-	# Get the summary
-	if (theSize == 1):
-		return "1 byte"
+
+
+
+#==============================================================================
+#		NData_Show : Show an NData.
+#------------------------------------------------------------------------------
+def NData_Show(theData, theInfo):
+
+	theFlags = getPathUInt(theData, "->mData.theFlags")
+	isLarge  = (theFlags & kNDataFlagIsLarge)
+
+	if (isLarge):
+		theSize = getPathUInt(theData, "->mData.Large.theSlice.mSize")
+		
+		if (theSize != 0):
+			theOffset = getPathUInt( theData, "->mData.Large.theSlice.mLocation")
+			thePtr    = getPathValue(theData, "->mData.Large.theState->theData")
+			theInfo   = ", data=" + hex(thePtr.GetValueAsUnsigned() + theOffset)
+
 	else:
-		return str(theSize) + " bytes"
+		theSize = (theFlags & kNDataFlagSmallSizeMask)
+		
+		if (theSize != 0):
+			sbData  = getPathData(theData, "->mData.Small.theData")
+			theInfo = ", data={0x" + ', 0x'.join(format(x, '02X') for x in sbData.uint8s[0:theSize]) + "}"
+
+	if (theSize == 0):
+		return "size=0"
+	else:
+		return "size=" + str(theSize) + theInfo
+
+
+
+
+
+#==============================================================================
+#		NRange_Show : Show an NRange.
+#------------------------------------------------------------------------------
+def NRange_Show(theRange, theInfo):
+
+	theLocation = getMemberUInt(theRange, "mLocation")
+	theSize     = getMemberUInt(theRange, "mSize")
+	
+	theLocation = "kNNotFound" if (theLocation == kNNotFound) else str(theLocation)
+	theSize     = "kNNotFound" if (theSize     == kNNotFound) else str(theSize)
+
+	return "location=" + theLocation + ", size=" + theSize
+
+
+
+
+
+#==============================================================================
+#		NString_Show : Show an NString.
+#------------------------------------------------------------------------------
+def NString_Show(theString, theInfo):
+
+	theFlags = getPathUInt(theString, "->mString.theFlags")
+	isLarge  = (theFlags & kNStringFlagIsLarge)
+	theInfo  = ""
+
+	if (isLarge):
+		# Decode the string
+		#
+		# Large strings are stored as a null-terminated buffer.
+		theEncoding = getPathUInt(theString, "->mString.Large.theState->stringData.theEncoding")
+		theData     = getPathValue(theString, "->mString.Large.theState->stringData.theData")
+
+		nullSize = getEncodingSize(theEncoding);
+		theBytes = NData_GetBytes(theData)
+		
+		theBytes = theBytes[:-nullSize];
+		theInfo  = getText(theBytes, theEncoding)
+
+
+
+		# Extract sub-strings
+		#
+		# A sub-string must be converted to UTF32, sliced by code points, then re-decoded.
+		stringSize = getPathUInt(theString, "->mString.Large.theState->theSize")
+		sliceSize  = getPathUInt(theString, "->mString.Large.theSlice.mSize")
+		
+		if (sliceSize != stringSize):
+			sliceOffset = getPathUInt(theString, "->mString.Large.theSlice.mLocation")
+
+			bytesUTF32 = theInfo.encode("utf-32");	
+			theFormat  = "=" + "I" * int(len(bytesUTF32) / 4);
+			codePoints = struct.unpack(theFormat, bytesUTF32);
+
+			sliceFirst = sliceOffset + 1;
+			sliceLast  = sliceFirst + sliceSize;
+			codePoints = codePoints[sliceFirst:sliceLast];
+
+			theFormat  = "=" + "I" * len(codePoints);
+			bytesUTF32 = struct.pack(theFormat, *codePoints);
+			theInfo    = getText(bytesUTF32, NStringEncoding_UTF32)
+
+	else:
+		theSize = (theFlags & kNStringFlagSmallSizeMask)
+		
+		if (theSize != 0):
+			sbData  = getPathData(theString, "->mString.Small.theData")
+			isUTF16 = (theFlags & kNStringFlagIsSmallUTF16)
+
+			if (isUTF16):
+				theEncoding = NStringEncoding_UTF16
+			else:
+				theEncoding = NStringEncoding_UTF8
+
+			byteSize = theSize * getEncodingSize(theEncoding)
+			theBytes = SBData_GetBytes(sbData, 0, byteSize)
+			theInfo  = getText(theBytes, theEncoding)
+
+	return '"' + theInfo + '"'
 
 
 
@@ -84,7 +354,10 @@ def SummariseNData(theValue, theInfo):
 #------------------------------------------------------------------------------
 def loadNano(theDebugger):
 
-	theDebugger.HandleCommand('type summary add -w Nano -F Nano.SummariseNData NData')
+	theDebugger.HandleCommand('type summary add -w Nano -F Nano.NData_Show   NData')
+	theDebugger.HandleCommand('type summary add -w Nano -F Nano.NRange_Show  NRange')
+	theDebugger.HandleCommand('type summary add -w Nano -F Nano.NString_Show NString')
+	theDebugger.HandleCommand('type category enable Nano')
 
 
 
@@ -94,6 +367,7 @@ def loadNano(theDebugger):
 #		__lldb_init_module : Entry point.
 #------------------------------------------------------------------------------
 def __lldb_init_module(theDebugger, theInfo):
+
 	loadNano(theDebugger)
 
 
