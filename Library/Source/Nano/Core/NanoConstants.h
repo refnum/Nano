8b576f8546204a879fbff791d62fc7673d3030c3
Dair Grant <dair@refnum.com>
2020-03-15 22:51:37 +0000

    Add signed integer range constants.

--- Library/Source/Nano/Core/NanoConstants.h
+++ Library/Source/Nano/Core/NanoConstants.h
@@ -64,10 +64,20 @@ static constexpr uint16_t kNUInt16Min                       = std::numeric_limit
 static constexpr uint32_t kNUInt32Min                       = std::numeric_limits<uint32_t>::min();
 static constexpr uint64_t kNUInt64Min                       = std::numeric_limits<uint64_t>::min();
 
+static constexpr int8_t  kNInt8Min                          = std::numeric_limits<int8_t>::min();
+static constexpr int16_t kNInt16Min                         = std::numeric_limits<int16_t>::min();
+static constexpr int32_t kNInt32Min                         = std::numeric_limits<int32_t>::min();
+static constexpr int64_t kNInt64Min                         = std::numeric_limits<int64_t>::min();
+
 static constexpr uint8_t  kNUInt8Max                        = std::numeric_limits<uint8_t>::max();
 static constexpr uint16_t kNUInt16Max                       = std::numeric_limits<uint16_t>::max();
 static constexpr uint32_t kNUInt32Max                       = std::numeric_limits<uint32_t>::max();
 static constexpr uint64_t kNUInt64Max                       = std::numeric_limits<uint64_t>::max();
+
+static constexpr int8_t  kNInt8Max                          = std::numeric_limits<int8_t>::max();
+static constexpr int16_t kNInt16Max                         = std::numeric_limits<int16_t>::max();
+static constexpr int32_t kNInt32Max                         = std::numeric_limits<int32_t>::max();
+static constexpr int64_t kNInt64Max                         = std::numeric_limits<int64_t>::max();
 
 static constexpr float32_t kNFloat32Max                     = std::numeric_limits<float32_t>::max();
 static constexpr float64_t kNFloat64Max                     = std::numeric_limits<float64_t>::max();
