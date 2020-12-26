88bb1f33469b996b3d70a0dc26b3f669c111b2f7
Dair Grant <dair@refnum.com>
2020-12-26 18:47:18 +0000

    Add NMutex benchmarks.

--- NanoTest/Source/Nano/Threads/TMutex.cpp
+++ NanoTest/Source/Nano/Threads/TMutex.cpp
@@ -40,8 +40,84 @@
 //		Includes
 //-----------------------------------------------------------------------------
 // Nano
+#include "NClock.h"
 #include "NMutex.h"
+#include "NScopedLock.h"
 #include "NTestFixture.h"
+#include "NThread.h"
+
+// System
+#include <thread>
+
+
+
+
+
+//=============================================================================
+//		Defines
+//-----------------------------------------------------------------------------
+#define BENCHMARK_NMUTEX                                    0
+
+
+
+
+
+//=============================================================================
+//		Internal Functions
+//-----------------------------------------------------------------------------
+template<typename MutexType, size_t kNumIters, size_t kNumThreads>
+NInterval BenchmarkMutex(std::function<void(MutexType&)> lockFunc,
+						 std::function<void(MutexType&)> unlockFunc)
+{
+
+
+	// Create the threads
+	std::atomic_bool     startFlag = false;
+	std::vector<NThread> theThreads;
+	MutexType            theMutexes[kNumThreads];
+
+	for (size_t t = 0; t < kNumThreads; t++)
+	{
+		theThreads.emplace_back(
+			NThread([&lockFunc, &unlockFunc, &startFlag, &theMutex = theMutexes[t]]() {
+				// Wait for the flag
+				std::mt19937 randomWork{std::random_device{}()};
+
+				while (!startFlag)
+				{
+					randomWork();
+				}
+
+
+				// Do the work
+				for (size_t n = 0; n < kNumIters; n++)
+				{
+					lockFunc(theMutex);
+
+					while (randomWork() % 4)
+					{
+					}
+
+					unlockFunc(theMutex);
+				}
+			}));
+	}
+
+
+
+	// Time the work
+	NThread::Sleep(0.5);
+
+	NClock theClock;
+	startFlag = true;
+
+	for (auto& theThread : theThreads)
+	{
+		theThread.WaitForCompletion();
+	}
+
+	return theClock.Stop();
+}
 
 
 
@@ -99,4 +175,92 @@ NANO_TEST(TMutex, "Recursion")
 
 	theLock.Unlock();
 	REQUIRE(!theLock.IsLocked());
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TMutex, "Benchmark/Uncontended")
+{
+#if BENCHMARK_NMUTEX
+
+
+	// Perform the test
+	constexpr size_t kNumIters   = 100000000;
+	constexpr size_t kNumThreads = 1;
+
+	printf("TMutex/Benchmark/Contended\n");
+	printf("%zu threads, %zu Iterations\n", kNumThreads, kNumIters);
+
+
+	NInterval timeNMutex = BenchmarkMutex<NMutex, kNumIters, kNumThreads>(
+		[](NMutex& theLock) {
+			theLock.Lock();
+		},
+		[](NMutex& theLock) {
+			theLock.Unlock();
+		});
+
+	printf("  NMutex     = %.6f\n", timeNMutex);
+
+
+	NInterval timeStdMutex = BenchmarkMutex<std::mutex, kNumIters, kNumThreads>(
+		[](std::mutex& theLock) {
+			theLock.lock();
+		},
+		[](std::mutex& theLock) {
+			theLock.unlock();
+		});
+
+	printf("  std::mutex = %.6f\n", timeStdMutex);
+
+#endif
+}
+
+
+
+
+
+//=============================================================================
+//		Test case
+//-----------------------------------------------------------------------------
+NANO_TEST(TMutex, "Benchmark/Contended")
+{
+#if BENCHMARK_NMUTEX
+
+
+	// Perform the test
+	constexpr size_t kNumIters   = 10000000;
+	constexpr size_t kNumThreads = 4;
+
+	printf("TMutex/Benchmark/Contended\n");
+	printf("%zu threads, %zu Iterations\n", kNumThreads, kNumIters);
+
+
+	NInterval timeNMutex = BenchmarkMutex<NMutex, kNumIters, kNumThreads>(
+		[](NMutex& theLock) {
+			theLock.Lock();
+		},
+		[](NMutex& theLock) {
+			theLock.Unlock();
+		});
+
+	printf("  NMutex     = %.6f\n", timeNMutex);
+
+
+	NInterval timeStdMutex = BenchmarkMutex<std::mutex, kNumIters, kNumThreads>(
+		[](std::mutex& theLock) {
+			theLock.lock();
+		},
+		[](std::mutex& theLock) {
+			theLock.unlock();
+		});
+
+	printf("  std::mutex = %.6f\n", timeStdMutex);
+
+#endif
 }
