bff11a75567244aa1182c9f6f1eb81c2b4bfd880
Dair Grant <dair@refnum.com>
2021-02-19 01:24:15 +0000

    Add NDataEncoder.

--- Library/Source/Nano/System/NDataEncoder.cpp
+++ Library/Source/Nano/System/NDataEncoder.cpp
@@ -3,102 +3,126 @@
 
 	DESCRIPTION:
 		Data encoder.
-	
+
 	COPYRIGHT:
-		Copyright (c) 2006-2013, refNum Software
-		<http://www.refnum.com/>
+		Copyright (c) 2006-2021, refNum Software
+		All rights reserved.
 
-		All rights reserved. Released under the terms of licence.html.
-	__________________________________________________________________________
+		Redistribution and use in source and binary forms, with or without
+		modification, are permitted provided that the following conditions
+		are met:
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+		(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+		OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	___________________________________________________________________________
 */
-//============================================================================
-//		Include files
-//----------------------------------------------------------------------------
-#include "cencode.h"
-#include "cdecode.h"
-
+//=============================================================================
+//		Includes
+//-----------------------------------------------------------------------------
 #include "NDataEncoder.h"
 
+// Nano
+#include "NData.h"
+#include "NString.h"
+#include "Nano_libb64.h"
 
 
 
 
-//============================================================================
-//		NDataEncoder::NDataEncoder : Constructor.
+
+//=============================================================================
+//		Internal Functions
+//-----------------------------------------------------------------------------
+//		HexToNibble : Convert a hex character to a nibble.
 //----------------------------------------------------------------------------
-NDataEncoder::NDataEncoder(void)
+constexpr uint8_t HexToNibble(utf8_t hexChar)
 {
+
+
+	// Decode the character
+	if (hexChar >= '0' && hexChar <= '9')
+	{
+		return uint8_t(hexChar - '0');
+	}
+	else if (hexChar >= 'A' && hexChar <= 'F')
+	{
+		return uint8_t(hexChar - 'A' + 10);
+	}
+	else if (hexChar >= 'a' && hexChar <= 'f')
+	{
+		return uint8_t(hexChar - 'a' + 10);
+	}
+
+	return 0;
 }
 
 
 
 
 
-//============================================================================
-//		NDataEncoder::~NDataEncoder : Destructor.
-//----------------------------------------------------------------------------
-NDataEncoder::~NDataEncoder(void)
-{
-}
-
-
-
-
-
-//============================================================================
+#pragma mark NDataEncoder
+//=============================================================================
 //		NDataEncoder::Encode : Encode to text.
-//----------------------------------------------------------------------------
-NString NDataEncoder::Encode(const NData &theValue, NDataEncoding theEncoding)
-{	NString		theResult;
-
+//-----------------------------------------------------------------------------
+NString NDataEncoder::Encode(NDataEncoding theEncoding, const NData& theValue)
+{
 
 
 	// Encode the data
-	switch (theEncoding) {
-		case kNDataEncodingHex:
-			theResult = Hex_Encode(theValue);
+	switch (theEncoding)
+	{
+		case NDataEncoding::Hex:
+			return Hex_Encode(theValue);
 			break;
 
-		case kNDataEncodingB64:
-			theResult = B64_Encode(theValue);
+		case NDataEncoding::Base64:
+			return B64_Encode(theValue);
 			break;
-
-		default:
-			NN_LOG("Unknown encoding: %d", theEncoding);
-			break;
-		}
-	
-	return(theResult);
+	}
 }
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		NDataEncoder::Decode : Decode from text.
-//----------------------------------------------------------------------------
-NData NDataEncoder::Decode(const NString &theValue, NDataEncoding theEncoding)
-{	NData	theResult;
+//-----------------------------------------------------------------------------
+NData NDataEncoder::Decode(NDataEncoding theEncoding, const NString& theValue)
+{
 
 
-
-	// Decode the string
-	switch (theEncoding) {
-		case kNDataEncodingHex:
-			theResult = Hex_Decode(theValue);
+	// Decide the data
+	switch (theEncoding)
+	{
+		case NDataEncoding::Hex:
+			return Hex_Decode(theValue);
 			break;
 
-		case kNDataEncodingB64:
-			theResult = B64_Decode(theValue);
+		case NDataEncoding::Base64:
+			return B64_Decode(theValue);
 			break;
-
-		default:
-			NN_LOG("Unknown encoding: %d", theEncoding);
-			break;
-		}
-	
-	return(theResult);
+	}
 }
 
 
@@ -106,164 +130,151 @@ NData NDataEncoder::Decode(const NString &theValue, NDataEncoding theEncoding)
 
 
 #pragma mark private
-//============================================================================
+//=============================================================================
 //		NDataEncoder::Hex_Encode : Encode to hex.
-//----------------------------------------------------------------------------
-NString NDataEncoder::Hex_Encode(const NData &theValue)
-{	NIndex			n, theSize;
-	NData			tmpBuffer;
-	NString			theResult;
-	const uint8_t	*dataPtr;
-	char			*textPtr;
-
+//-----------------------------------------------------------------------------
+NString NDataEncoder::Hex_Encode(const NData& theValue)
+{
 
 
 	// Get the state we need
-	theSize = theValue.GetSize();
-	dataPtr = theValue.GetData();
-	textPtr = (char *) tmpBuffer.AppendData((theSize * 2) + 1);
+	const uint8_t* srcBytes = theValue.GetData();
+	size_t         srcSize  = theValue.GetSize();
 
-	if (theSize == 0 || dataPtr == NULL || textPtr == NULL)
-		return(theResult);
-
+	NData   dstData(srcSize * 2, nullptr, NDataSource::None);
+	utf8_t* dstChars = reinterpret_cast<utf8_t*>(dstData.GetMutableData());
 
 
-	// Convert to a string
-	for (n = 0; n < theSize; n++)
-		{
-		sprintf(textPtr, "%.2X", dataPtr[n]);
-		textPtr += 2;
-		}
-	
-	theResult = NString(tmpBuffer);
-	
-	return(theResult);
+
+	// Encode the bytes
+	for (size_t n = 0; n < srcSize; n++)
+	{
+		uint8_t nibbleHi = (srcBytes[n] >> 4) & 0x0F;
+		uint8_t nibbleLo = (srcBytes[n] >> 0) & 0x0F;
+
+		dstChars[(n * 2) + 0] = "0123456789ABCDEF"[nibbleHi];
+		dstChars[(n * 2) + 1] = "0123456789ABCDEF"[nibbleLo];
+	}
+
+	return NString(NStringEncoding::UTF8, dstData);
 }
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		NDataEncoder::Hex_Decode : Decode from hex.
-//----------------------------------------------------------------------------
-NData NDataEncoder::Hex_Decode(const NString &theValue)
-{	NIndex			n, theSize;
-	NData			theResult;
-	uint8_t			*dataPtr;
-	const char		*textPtr;
-	unsigned int	byteVal;
-
+//-----------------------------------------------------------------------------
+NData NDataEncoder::Hex_Decode(const NString& theValue)
+{
 
 
 	// Validate our parameters
-	NN_ASSERT(theValue.IsEmpty() || (theValue.GetSize() % 2) == 0);
+	NN_EXPECT((theValue.GetSize() % 2) == 0, "Input is one nibble short!");
 
 
 
 	// Get the state we need
-	theSize = theValue.GetSize() / 2;
-	dataPtr = theResult.AppendData(theSize);
-	textPtr = theValue.GetUTF8();
+	const utf8_t* srcChars = theValue.GetUTF8();
+	size_t        srcSize  = strlen(srcChars);
 
-	if (theSize == 0 || dataPtr == NULL || textPtr == NULL)
-		return(theResult);
+	NData    dstData(srcSize / 2, nullptr, NDataSource::None);
+	uint8_t* dstBytes = dstData.GetMutableData();
+	size_t   dstSize  = dstData.GetSize();
 
 
 
-	// Convert the string
-	//
-	// Visual Studio does not support 'hh' correctly, so we need to read byte
-	// values into a temporary variable:
-	//
-	// https://connect.microsoft.com/VisualStudio/feedback/details/416843/sscanf-cannot-not-handle-hhd-format
-	for (n = 0; n < theSize; n++)
-		{
-		sscanf(textPtr, "%2x", &byteVal);
-		NN_ASSERT((byteVal & 0xFFFFFF00) == 0);
+	// Decode the hex
+	for (size_t n = 0; n < dstSize; n++)
+	{
+		utf8_t hexHi = srcChars[(n * 2) + 0];
+		utf8_t hexLo = srcChars[(n * 2) + 1];
 
-		dataPtr[n] = (uint8_t) byteVal;
-		textPtr   += 2;
-		}
-	
-	return(theResult);
+		dstBytes[n] = uint8_t((HexToNibble(hexHi) << 4) | HexToNibble(hexLo));
+	}
+
+	return dstData;
 }
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		NDataEncoder::B64_Encode : Encode to Base64.
-//----------------------------------------------------------------------------
-NString NDataEncoder::B64_Encode(const NData &theValue)
-{	NData					theBuffer;
-	NString					theString;
-	base64_encodestate		theState;
-	char					*dataPtr;
-	NIndex					dataSize;
-
+//-----------------------------------------------------------------------------
+NString NDataEncoder::B64_Encode(const NData& theValue)
+{
 
 
 	// Get the state we need
-	base64_init_encodestate(&theState);
+	const uint8_t* srcBytes = theValue.GetData();
+	size_t         srcSize  = theValue.GetSize();
 
-	if (theValue.IsEmpty() || !theBuffer.SetSize(theValue.GetSize() * 2))
-		return(theString);
-
-
-
-	// Encode the value
-	dataPtr   = (char *) theBuffer.GetData();
-	dataSize  = base64_encode_block((const char *) theValue.GetData(), theValue.GetSize(), dataPtr, &theState);
-	theString = NString(dataPtr, dataSize);
-
-	dataSize = base64_encode_blockend(dataPtr, &theState);
-	if (dataSize != 0)
-		theString += NString(dataPtr, dataSize);
+	NData dstData(srcSize * 2, nullptr, NDataSource::None);
+	char* dstChars = reinterpret_cast<char*>(dstData.GetMutableData());
 
 
 
-	// Remove the trailing newline
-	NN_ASSERT(theString.GetRight(1) == "\n");
-	theString.TrimRight(1);
+	// Encode the bytes
+	base64_encodestate stateB64{};
+	size_t             dstSize = 0;
 
-	return(theString);
+	base64_init_encodestate(&stateB64);
+	stateB64.chars_per_line = 72;
+
+	dstSize += base64_encode_block(srcBytes, srcSize, dstChars, &stateB64);
+	dstSize += base64_encode_blockend(dstChars + dstSize, &stateB64);
+
+
+
+	// Trim newlines
+	//
+	// To ensure consistent output we always trim any trailing newline.
+	if (dstSize > 0 && dstChars[dstSize - 1] == '\n')
+	{
+		dstSize--;
+	}
+
+	dstData.SetSize(size_t(dstSize));
+
+	return NString(NStringEncoding::UTF8, dstData);
 }
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		NDataEncoder::B64_Decode : Decode from Base64.
-//----------------------------------------------------------------------------
-NData NDataEncoder::B64_Decode(const NString &theValue)
-{	NData					theData, valueData;
-	base64_decodestate		theState;
-	char					*dataPtr;
-	NIndex					dataSize;
-
+//-----------------------------------------------------------------------------
+NData NDataEncoder::B64_Decode(const NString& theValue)
+{
 
 
 	// Get the state we need
-	base64_init_decodestate(&theState);
+	const char* srcChars = reinterpret_cast<const char*>(theValue.GetUTF8());
+	size_t      srcSize  = strlen(srcChars);
 
-	valueData = theValue.GetData(kNStringEncodingUTF8);
-	
-	if (!theData.SetSize(valueData.GetSize()))
-		return(theData);
-
+	NData    dstData(srcSize, nullptr, NDataSource::None);
+	uint8_t* dstBytes = dstData.GetMutableData();
 
 
-	// Decode the value
-	dataPtr  = (char *) theData.GetData();
-	dataSize = base64_decode_block((const char *) valueData.GetData(), valueData.GetSize(), dataPtr, &theState);
 
-	theData.SetSize(dataSize);
+	// Decode the bytes
+	base64_decodestate stateB64;
 
-	return(theData);
+	base64_init_decodestate(&stateB64);
+
+	size_t dstSize = base64_decode_block(srcChars, srcSize, dstBytes, &stateB64);
+
+
+
+	// Trim the data
+	//
+	// Our output buffer is always over-sized so we trim to the actual size.
+	dstData.SetSize(dstSize);
+
+	return dstData;
 }
-
-
-
--- Library/Source/Nano/System/NDataEncoder.h
+++ Library/Source/Nano/System/NDataEncoder.h
@@ -3,67 +3,93 @@
 
 	DESCRIPTION:
 		Data encoder.
-	
+
 	COPYRIGHT:
-		Copyright (c) 2006-2013, refNum Software
-		<http://www.refnum.com/>
+		Copyright (c) 2006-2021, refNum Software
+		All rights reserved.
 
-		All rights reserved. Released under the terms of licence.html.
-	__________________________________________________________________________
+		Redistribution and use in source and binary forms, with or without
+		modification, are permitted provided that the following conditions
+		are met:
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+		(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+		OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	___________________________________________________________________________
 */
-#ifndef NDATAENCODER_HDR
-#define NDATAENCODER_HDR
-//============================================================================
-//		Include files
-//----------------------------------------------------------------------------
-#include "NString.h"
-#include "NData.h"
+#ifndef NDATA_ENCODER_H
+#define NDATA_ENCODER_H
+//=============================================================================
+//		Includes
+//-----------------------------------------------------------------------------
 
 
 
 
 
-//============================================================================
+//=============================================================================
 //		Constants
-//----------------------------------------------------------------------------
-// Data encodings
-typedef enum {
-	kNDataEncodingHex,
-	kNDataEncodingB64
-} NDataEncoding;
-
-
-
-
-
-//============================================================================
-//		Class declaration
-//----------------------------------------------------------------------------
-class NDataEncoder {
-public:
-										NDataEncoder(void);
-	virtual							   ~NDataEncoder(void);
-
-
-	// Encode/decode data
-	NString								Encode(const NData   &theValue, NDataEncoding theEncoding);
-	NData								Decode(const NString &theValue, NDataEncoding theEncoding);
-
-
-private:
-	NString								Hex_Encode(const NData   &theValue);
-	NData								Hex_Decode(const NString &theValue);
-
-	NString								B64_Encode(const NData   &theValue);
-	NData								B64_Decode(const NString &theValue);
-
-
+//-----------------------------------------------------------------------------
+// Encodings
+enum class NDataEncoding
+{
+	Hex,
+	Base64
 };
 
 
 
 
 
-#endif // NDATAENCODER_HDR
+//=============================================================================
+//		Types
+//-----------------------------------------------------------------------------
+// Forward declarations
+class NData;
+class NString;
 
 
+
+
+
+//=============================================================================
+//		Class Declaration
+//-----------------------------------------------------------------------------
+class NDataEncoder
+{
+public:
+	// Encode/decode data
+	static NString                      Encode(NDataEncoding theEncoding, const NData&   theValue);
+	static NData                        Decode(NDataEncoding theEncoding, const NString& theValue);
+
+
+private:
+	static NString                      Hex_Encode(const NData&   theValue);
+	static NData                        Hex_Decode(const NString& theValue);
+
+	static NString                      B64_Encode(const NData&   theValue);
+	static NData                        B64_Decode(const NString& theValue);
+};
+
+
+
+#endif // NDATA_ENCODER_H
