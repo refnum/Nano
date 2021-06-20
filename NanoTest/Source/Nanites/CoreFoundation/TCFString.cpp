33de05ed90ae2b70952a784e64d1ec70b1c39bb7
Dair Grant <dair@refnum.com>
2021-06-20 14:37:08 +0100

    Apply rn-format.

--- NanoTest/Source/Nanites/CoreFoundation/TCFString.cpp
+++ NanoTest/Source/Nanites/CoreFoundation/TCFString.cpp
@@ -5,50 +5,75 @@
 		NCFString tests.
 
 	COPYRIGHT:
-		Copyright (c) 2006-2013, refNum Software
-		<http://www.refnum.com/>
+		Copyright (c) 2006-2021, refNum Software
+		All rights reserved.
 
-		All rights reserved. Released under the terms of licence.html.
-	__________________________________________________________________________
+		Redistribution and use in source and binary forms, with or without
+		modification, are permitted provided that the following conditions
+		are met:
+		
+		1. Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+		
+		2. Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+		
+		3. Neither the name of the copyright holder nor the names of its
+		contributors may be used to endorse or promote products derived from
+		this software without specific prior written permission.
+		
+		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+		"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+		A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+		HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+		SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+		LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+		DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+		THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+		(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+		OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	___________________________________________________________________________
 */
-//============================================================================
-//		Include files
-//----------------------------------------------------------------------------
-#include "NTestFixture.h"
+//=============================================================================
+//		Includes
+//-----------------------------------------------------------------------------
 #include "NCFString.h"
+#include "NTestFixture.h"
 
 
 
 
 
-//============================================================================
-//		Internal constants
-//----------------------------------------------------------------------------
-static const NString     kTestNString								= "Hello World";
-static const CFStringRef kTestCFString								= CFSTR("Hello World");
+//=============================================================================
+//		Internal Constants
+//-----------------------------------------------------------------------------
+static const NString     kTestNString                       = "Hello World";
+static const CFStringRef kTestCFString                      = CFSTR("Hello World");
 
 
 
 
 
-//============================================================================
-//		Test fixture
-//----------------------------------------------------------------------------
-#define TEST_NCFSTRING(...)											TEST_NANO(TCFString, ##__VA_ARGS__)
+//=============================================================================
+//		Test Fixture
+//-----------------------------------------------------------------------------
+#define TEST_NCFSTRING(...)                                 TEST_NANO(TCFString, ##__VA_ARGS__)
 
 FIXTURE_NANO(TCFString)
 {
-	NCFString		theString;
-	NCFObject		cfString;
+	NCFString theString;
+	NCFObject cfString;
 };
 
 
 
 
 
-//============================================================================
-//		Test case
-//----------------------------------------------------------------------------
+//=============================================================================
+//		Test Case
+//-----------------------------------------------------------------------------
 TEST_NCFSTRING("Assign", "[cf]")
 {
 
@@ -67,5 +92,3 @@ TEST_NCFSTRING("Assign", "[cf]")
 	theString.SetObject(CFStringCreateCopy(kCFAllocatorDefault, kTestCFString), true);
 	REQUIRE(theString == kTestNString);
 }
-
-
